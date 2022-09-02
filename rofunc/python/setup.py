b28b405073cea48975ea3728d67898b6e4378a15
skylark <l.syj@163.com>
2022-09-02 09:49:51 +0000

    Revert "add dist"
    This reverts commit 444232d9b35d93ee81d4178b49aa4b5637bf9418.

--- rofunc/python/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="rofunc",
-    version="0.1",
-    author="skylark",
-    author_email="jjliu@mae.cuhk.edu.hk",
-    packages=find_packages(),
-)
