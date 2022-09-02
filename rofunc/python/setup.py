e6091678e3722c2845825d4dc57206a21faa2e73
skylark <l.syj@163.com>
2022-09-02 13:47:39 +0800

    Revert "add dist"
    This reverts commit 598c21946466ec1ae77878b00629e65c88c622d9.

--- rofunc/python/setup.py
+++ rofunc/python/setup.py
@@ -5,7 +5,5 @@ setup(
     version="0.1",
     author="skylark",
     author_email="jjliu@mae.cuhk.edu.hk",
-    requires= ['numpy','matplotlib', 'mvnx'],
     packages=find_packages(),
-    license="apache 3.0"
 )
