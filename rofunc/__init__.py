d08eab6719f7842555f1b8b24785f715b86a348a
skylark <l.syj@163.com>
2022-09-12 14:46:12 +0800

    update

--- rofunc/__init__.py
+++ rofunc/__init__.py
@@ -3,4 +3,4 @@ from __future__ import absolute_import
 from .devices import zed, xsens, optitrack, mmodal, emg
 from .planning import lqt
 from .lfd import tpgmm
-from .utils import *
+from .utils import bezier, visualab, coord
