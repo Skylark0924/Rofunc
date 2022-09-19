336eec79464a7384f258eca08f83e5e4c420fee9
skylark <l.syj@163.com>
2022-09-19 12:36:45 +0000

    Update doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -68,10 +68,6 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_theme_options = {
-    "rightsidebar": "true",
-    "relbarbgcolor": "black"
-}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
