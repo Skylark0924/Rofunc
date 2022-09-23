c723bcea1ca1dc42d6c446b265b1091c46577be0
skylark <l.syj@163.com>
2022-09-23 13:37:53 +0000

    add dualfranka and curi to the simulator module

--- rofunc/__init__.py
+++ rofunc/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 
 from .devices import zed, xsens, optitrack, mmodal, emg
-from .lfd import tpgmm, gmr, tpgmr
 from .planning import lqt
+from .lfd import tpgmm, gmr, tpgmr
 from .utils import visualab, coord, data_generator
-from .simulator import franka
