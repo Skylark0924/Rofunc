7a1910b26a4029dc43971592b889ce90f93c1fc8
Skylark <l.syj@163.com>
2022-09-29 17:51:58 +0800

    fix record bug

--- rofunc/devices/optitrack/record.py
+++ rofunc/devices/optitrack/record.py
@@ -15,21 +15,23 @@ def data_process(data):
 
 def opti_run(root_dir, exp_name, ip, port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.settimeout(20.0)
+    # client.settimeout(20.0)
     client.connect((ip, port))
     print("Connected to socket: {}:{}".format(ip, port))
     # receive optitrack data
+    opti_data = np.array([])
     while True:
         utf_data = client.recv(1024).decode("utf-8")
+        print(utf_data)
         raw_position, raw_orientation = data_process(utf_data)
         client.send("ok".encode("utf-8"))
         raw_pose = np.append(raw_position, raw_orientation)
-        opti_data = np.append(raw_pose, axis=0)
+        opti_data = np.append(opti_data, raw_pose, axis=0)
 
         # save optitrack data to npy file
-        if keyboard.read_key() == 's':
-            np.save(root_dir + exp_name + 'opti_data.npy', opti_data)
-            break
+        # if keyboard.read_key() == 's':
+        np.save(root_dir + '/' + exp_name + '/' + 'opti_data.npy', opti_data)
+        # break
 
 
 def record(root_dir, exp_name, ip, port):
@@ -55,12 +57,16 @@ def record(root_dir, exp_name, ip, port):
 
 
 if __name__ == "__main__":
-    root_dir = './'
-    ip = ''
-    port = ''
+    root_dir = '/home/skylark/Data/optitrack_record'
+    ip = '192.168.13.118'
+    port = 6688
 
     from datetime import datetime
     exp_name = datetime.now().strftime('%Y%m%d_%H%M%S')
 
-    import rofunc as rf
-    rf.optitrack.record(root_dir, exp_name, ip, port)
+    # import rofunc as rf
+    record(root_dir, exp_name, ip, port)
+    # filename = '/home/skylark/Data/optitrack_record/20220929_134531/opti_data.npy'
+    # data = np.load(filename).reshape((-1, 7))
+    # print(data[7:])
+
