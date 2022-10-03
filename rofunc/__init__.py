3fb3a02bbbd5b80df3301f8f188aa8e578766d39
skylark <l.syj@163.com>
2022-10-03 10:52:51 +0000

    new file system of lfd

--- rofunc/__init__.py
+++ rofunc/__init__.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import
 
 from .devices import zed, xsens, optitrack, mmodal, emg
-from .lfd import tpgmm, gmr, tpgmr
+from .lfd import ml, dl, rl
 from .planning import lqt
 from .utils import visualab, coord, data_generator, params
 from .simulator import franka, dualfranka, curi, walker
+
+from .lfd.ml import tpgmm, gmr, tpgmr
+from .lfd.rl import ppo, dqn
---
+++ rofunc/lfd/ml/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from . import gmr
+from . import tpgmm
+from . import tpgmr
