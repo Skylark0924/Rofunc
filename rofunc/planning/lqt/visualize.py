f6e679407ac8e14663dd79aa9b5afede8ba13d59
skylark <l.syj@163.com>
2022-10-03 15:51:52 +0000

    refine the traj visualab

--- rofunc/planning/lqt/visualize.py
+++ rofunc/planning/lqt/visualize.py
@@ -1,12 +1,7 @@
-import random
-
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.axes._axes import _log as matplotlib_axes_logger
-from matplotlib.pyplot import cm
+import rofunc as rf
 from pytransform3d.rotations import matrix_from_quaternion, plot_basis
-
-matplotlib_axes_logger.setLevel('ERROR')
 
 
 def plot_2d(param, x_hat_l, x_hat_r, idx_slices, tl, via_point_l, via_point_r):
@@ -69,39 +64,33 @@ def plot_2d(param, x_hat_l, x_hat_r, idx_slices, tl, via_point_l, via_point_r):
     plt.show()
 
 
-def plot_3d_uni(x_hat, muQ=None, idx_slices=None, ori=False, save=False, save_file_name=None, ax=None):
-    if ax is None:
+def plot_3d_uni(x_hat, muQ=None, idx_slices=None, ori=False, save=False, save_file_name=None, g_ax=None):
+    if g_ax is None:
         fig = plt.figure(figsize=(4, 4))
         ax = fig.add_subplot(111, projection='3d', fc='white')
+    else:
+        ax = g_ax
 
     if muQ is not None and idx_slices is not None:
         for slice_t in idx_slices:
             ax.scatter(muQ[slice_t][0], muQ[slice_t][1], muQ[slice_t][2], c='red', s=10)
 
-    # Plot 3d trajectories
-    ax.plot(x_hat[:, 0], x_hat[:, 1], x_hat[:, 2], c='black')
-
-    # Starting points
-    ax.scatter(x_hat[0, 0], x_hat[0, 1], x_hat[0, 2], c='black', s=20)
-
-    if ori:
-        ori = x_hat[:, 3:7]
-        for t in range(0, len(ori), 10):
-            R = matrix_from_quaternion(ori[t])
-            p = x_hat[t, :3]
-            ax = plot_basis(ax=ax, R=R, p=p, s=0.001)
+    rf.visualab.traj_plot(x_hat, mode='3d', ori=ori, g_ax=ax)
 
     if save:
         assert save_file_name is not None
         np.save(save_file_name, np.array(x_hat))
-    plt.show()
+    if g_ax is None:
+        plt.show()
 
 
-def plot_3d_bi(x_hat_l, x_hat_r, muQ_l=None, muQ_r=None, idx_slices=None, ori=True, save=False, save_file_name=None,
-               ax=None):
-    if ax is None:
+def plot_3d_bi(x_hat_l, x_hat_r, muQ_l=None, muQ_r=None, idx_slices=None, ori=False, save=False, save_file_name=None,
+               g_ax=None):
+    if g_ax is None:
         fig = plt.figure(figsize=(4, 4))
-        ax = fig.add_subplot(111, projection='3d')
+        ax = fig.add_subplot(111, projection='3d', fc='white')
+    else:
+        ax = g_ax
 
     if muQ_l is not None and muQ_r is not None and idx_slices is not None:
         for slice_t in idx_slices:
@@ -113,38 +102,12 @@ def plot_3d_bi(x_hat_l, x_hat_r, muQ_l=None, muQ_r=None, idx_slices=None, ori=Tr
             x_hat_l = np.expand_dims(x_hat_l, axis=0)
             x_hat_r = np.expand_dims(x_hat_r, axis=0)
 
-    for i in range(len(x_hat_l)):
-        c_l = cm.tab20c(random.random())
-        c_r = cm.tab20b(random.random())
-
-        # Plot 3d trajectories
-        ax.plot(x_hat_l[i][:, 0], x_hat_l[i][:, 1], x_hat_l[i][:, 2], label='left arm', c=c_l)
-        ax.plot(x_hat_r[i][:, 0], x_hat_r[i][:, 1], x_hat_r[i][:, 2], label='right arm', c=c_r)
-
-        # Starting points
-        ax.scatter(x_hat_l[i][0, 0], x_hat_l[i][0, 1], x_hat_l[i][0, 2], s=20, label='left start point', c=c_l)
-        ax.scatter(x_hat_r[i][0, 0], x_hat_r[i][0, 1], x_hat_r[i][0, 2], s=20, label='right start point', c=c_r)
-
-        # End points
-        ax.scatter(x_hat_l[i][-1, 0], x_hat_l[i][-1, 1], x_hat_l[i][-1, 2], marker='x', s=20, c=c_l,
-                   label='left end point')
-        ax.scatter(x_hat_r[i][-1, 0], x_hat_r[i][-1, 1], x_hat_r[i][-1, 2], marker='x', s=20, c=c_r,
-                   label='right end point')
-
-        if ori:
-            l_ori = x_hat_l[i][:, 3:7]
-            r_ori = x_hat_r[i][:, 3:7]
-            for t in range(len(l_ori)):
-                R_l = matrix_from_quaternion(l_ori[t])
-                R_r = matrix_from_quaternion(r_ori[t])
-                p_l = x_hat_l[i][t, :3]
-                p_r = x_hat_r[i][t, :3]
-                ax = plot_basis(ax=ax, R=R_l, p=p_l, s=0.01)
-                ax = plot_basis(ax=ax, R=R_r, p=p_r, s=0.01)
+    rf.visualab.traj_plot(x_hat_l, title='left arm', mode='3d', ori=ori, g_ax=ax)
+    rf.visualab.traj_plot(x_hat_r, title='right arm', mode='3d', ori=ori, g_ax=ax)
 
     if save:
         assert save_file_name is not None
         np.save(save_file_name[0], np.array(x_hat_l))
         np.save(save_file_name[1], np.array(x_hat_r))
-    ax.legend()
-    plt.show()
+    if g_ax is None:
+        plt.show()
