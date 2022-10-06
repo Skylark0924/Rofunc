d94acdf4a2b5dd8be1a7ecbe4fc2eddc83fbbb27
Chenzui Li <lichenzui1995@gmail.com>
2022-10-06 09:35:02 +0000

    update 'process' and 'record'

--- rofunc/devices/emg/record.py
+++ rofunc/devices/emg/record.py
@@ -1,34 +1,41 @@
-from .src import pytrigno
+from src import pytrigno
 import argparse
-
-def record(host):
+import numpy as np
+import os.path
+def record(host, n, t):
     dev = pytrigno.TrignoEMG(channel_range=(0, 0), samples_per_read=1000,
                              host=host)
 
-    # test single-channel
+    # test n-channel
+    dev.set_channel_range((0, n-1))
     dev.start()
-    # for i in range(4):
-    while True:
-        data = dev.read()
-        print(data)
-        assert data.shape == (1, 1000)
-    print('single-channel achieved')
-    dev.stop()
 
-    # test multi-channel
-    # dev.set_channel_range((0, 5))
-    # dev.start()
-    # for i in range(10):
-    #     data = dev.read()
-    #     print(data * 1e12)
-    #     assert data.shape == (6, 270)
-    # print('multi-channel achieved')
-    #
+    data_sensor_1 = []
+    data_sensor_2 = []
+    data_sensor_3 = []
+    data_sensor_4 = []
+    data_sensor_5 = []
+    data_sensor_6 = []
+    for i in range(int(t)):
     # while True:
-    #     data = dev.read() * 1e10
-    #     assert data.shape == (6, 270)
-    #     print(data[0])
-    # dev.stop()
+        data = dev.read() * 1e6
+        print(data)
+        assert data.shape == (n, 1000)
+        data_1 = data[0:1]
+        data_2 = data[1:2]
+        data_3 = data[2:3]
+        data_4 = data[3:4]
+        data_5 = data[4:5]
+        data_6 = data[5:6]
+        data_sensor_1.append(data_1)
+        data_sensor_2.append(data_2)
+        data_sensor_3.append(data_3)
+        data_sensor_4.append(data_4)
+        data_sensor_5.append(data_5)
+        data_sensor_6.append(data_6)
+    print(n,'-channel achieved')
+    dev.stop()
+    return data_sensor_1, data_sensor_2, data_sensor_3, data_sensor_4, data_sensor_5, data_sensor_6
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -36,8 +43,14 @@ if __name__ == '__main__':
     parser.add_argument(
         '-a', '--addr',
         dest='host',
-        default='10.13.58.70',
+        default='10.13.166.60',
         help="IP address of the machine running TCU. Default is localhost.")
     args = parser.parse_args()
 
-    record(args.host)
+    n = 6
+    t = 30
+    data_sensor_1, data_sensor_2, data_sensor_3, data_sensor_4, data_sensor_5, data_sensor_6 = record(args.host, n, t)
+    data_sensor_4 = np.reshape(np.array(data_sensor_4), -1)
+    # print(data_sensor_4)
+    np.save("data_sensor_4_time_{}.npy".format(t), data_sensor_4)
+    # np.savetxt('data_sensor_4_time_{}.txt'.format(t), data_sensor_4, delimiter=',', fmt='%.08f')
