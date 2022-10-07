fa053b97d43b0ab82fc48f1f68fc151a5d6faffc
Chenzui Li <lichenzui1995@gmail.com>
2022-10-07 08:45:18 +0000

    emg_record_full_version

--- rofunc/devices/emg/record.py
+++ rofunc/devices/emg/record.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 import numpy as np
 
@@ -6,39 +7,36 @@ from src import pytrigno
 
 
 def record(host, n, t):
-    dev = pytrigno.TrignoEMG(channel_range=(0, 0), samples_per_read=1000,
+    """
+    Communication with and data acquisition from a Delsys Trigno wireless EMG system.
+    Delsys Trigno Control Utility needs to be installed and running on, and the device
+    needs to be plugged in. Records can be run with a device connected to a remote machine.
+
+    Args:
+        host: host of a remote machine
+        n: number of emg channels
+        t: running time
+
+    Returns: None
+    """
+    SAMPLES_PER_READ = 2000
+    dev = pytrigno.TrignoEMG(channel_range=(0, 0), samples_per_read=SAMPLES_PER_READ,
                              host=host)
 
-    # test n-channel
     dev.set_channel_range((0, n - 1))
     dev.start()
-
-    data_sensor_1 = []
-    data_sensor_2 = []
-    data_sensor_3 = []
-    data_sensor_4 = []
-    data_sensor_5 = []
-    data_sensor_6 = []
+    data_sensor = []
     for i in range(int(t)):
         # while True:
         data = dev.read() * 1e6
         print(data)
-        assert data.shape == (n, 1000)
-        data_1 = data[0:1]
-        data_2 = data[1:2]
-        data_3 = data[2:3]
-        data_4 = data[3:4]
-        data_5 = data[4:5]
-        data_6 = data[5:6]
-        data_sensor_1.append(data_1)
-        data_sensor_2.append(data_2)
-        data_sensor_3.append(data_3)
-        data_sensor_4.append(data_4)
-        data_sensor_5.append(data_5)
-        data_sensor_6.append(data_6)
+        assert data.shape == (n, SAMPLES_PER_READ)
+        data_sensor.append(data)
     print(n, '-channel achieved')
     dev.stop()
-    return data_sensor_1, data_sensor_2, data_sensor_3, data_sensor_4, data_sensor_5, data_sensor_6
+
+    data_sensor = np.reshape(np.transpose(np.array(data_sensor), (0, 2, 1)), (-1, n))
+    np.save(os.path.join('./data', 'emg_data.npy'), data_sensor)
 
 
 if __name__ == '__main__':
@@ -51,15 +49,6 @@ if __name__ == '__main__':
         help="IP address of the machine running TCU. Default is localhost.")
     args = parser.parse_args()
 
-    n = 6
-    t = 30
-    data_sensor_1, data_sensor_2, data_sensor_3, data_sensor_4, data_sensor_5, data_sensor_6 = record(args.host, n, t)
-    data_sensor_1 = np.reshape(np.array(data_sensor_1), -1)
-    data_sensor_2 = np.reshape(np.array(data_sensor_2), -1)
-    data_sensor_3 = np.reshape(np.array(data_sensor_3), -1)
-    data_sensor_4 = np.reshape(np.array(data_sensor_4), -1)
-    data_sensor_5 = np.reshape(np.array(data_sensor_5), -1)
-    data_sensor_6 = np.reshape(np.array(data_sensor_6), -1)
-    # print(data_sensor_4)
-    np.save("data_sensor_4_time_{}.npy".format(t), data_sensor_4)
-    # np.savetxt('data_sensor_4_time_{}.txt'.format(t), data_sensor_4, delimiter=',', fmt='%.08f')
+    # For instance, 4 channels and 10 seconds are chosen.
+    import rofunc as rf
+    rf.emg.record(args.host, 4, 10)
