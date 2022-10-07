21a878462aceff5caee5cd6189a84ea45a7df0f4
Chenzui Li <lichenzui1995@gmail.com>
2022-10-07 08:45:18 +0000

    emg_record_full_version

--- rofunc/devices/emg/process.py
+++ rofunc/devices/emg/process.py
@@ -48,41 +48,48 @@ def activationlevel(data, MVC):
     A = np.array(A)
     return A
 
+def process(data, n):
+    """
+    Filter the original EMG signals (from the Delsys system, 1000 Hz) to the desired frequency
+    """
+    data_filter = []
+    for i in range(0, len(data) - n + 1, n):
+        data_new = data[i]
+        data_filter.append(data_new)
+    data_filter = np.array(data_filter)
 
 if __name__ == '__main__':
-    a = 20
-    b = 0.05
+    # FRAME_SIZE = 64
+    # HOP_LENGTH = 32
+    SAMPING_RATE = 2000
     n = 4
-    FRAME_SIZE = 64
-    HOP_LENGTH = 32
-    SAMPING_RATE = 1000
     # Generate 10 seconds of EMG signal (recorded at 250 samples / second)
-    emg_1 = nk.emg_simulate(duration=10, sampling_rate=SAMPING_RATE, burst_number=3)
-    # emg_1 = np.load('data_sensor_4_time_30.npy')
-    emg_2 = nk.emg_simulate(duration=10, sampling_rate=SAMPING_RATE, burst_number=2)
+    # emg_1 = nk.emg_simulate(duration=10, sampling_rate=SAMPING_RATE, burst_number=3)
+    # emg_2 = nk.emg_simulate(duration=10, sampling_rate=SAMPING_RATE, burst_number=2)
+    emg = np.load('./data/emg_data.npy')
 
-    emg_filter_1 = filtering(emg_1, n)
-    emg_filter_2 = filtering(emg_2, n)
+    emg_filter_1 = filtering(emg[:, 0], n)
+    emg_filter_2 = filtering(emg[:, 1], n)
 
     EMG_clean_1, MVC_1, signals_1 = processing(emg_filter_1, int(SAMPING_RATE / n))
     EMG_clean_2, MVC_2, signals_2 = processing(emg_filter_2, int(SAMPING_RATE / n))
 
     fig, (ax0, ax1) = plt.subplots(nrows=2, ncols=1, sharex=True)
-    ax1.set_xlabel("Time (seconds)", fontweight="bold", fontdict={'family': 'Times New Roman', 'size': 20}, fontsize=12)
-    fig.suptitle("Raw and Clean EMG Signals", fontweight="bold", fontdict={'family': 'Times New Roman', 'size': 20},
+    ax1.set_xlabel("Time (seconds)", fontweight="bold", fontdict={'family': 'Times New Roman'}, fontsize=12)
+    fig.suptitle("Raw and Clean EMG Signals", fontweight="bold", fontdict={'family': 'Times New Roman'},
                  fontsize=16)
     plt.subplots_adjust(hspace=0.2)
     x_axis = np.linspace(0, emg_filter_1.shape[0] / int(SAMPING_RATE / n), emg_filter_1.shape[0])
     # Plot cleaned and raw EMG.
     legend_font = {"family": "Times New Roman"}
-    ax0.set_title("Sensor_1", fontdict={'family': 'Times New Roman', 'size': 20}, fontsize=12)
+    ax0.set_title("Sensor_1", fontdict={'family': 'Times New Roman'}, fontsize=12)
     ax0.plot(x_axis, emg_filter_1, color="#B0BEC5", label="Raw", zorder=1)
     ax0.plot(
         x_axis, EMG_clean_1, color="#FFC107", label="Cleaned", zorder=1, linewidth=1.5
     )
     ax0.legend(loc="upper right", frameon=True, prop=legend_font)
 
-    ax1.set_title("Sensor_2", fontdict={'family': 'Times New Roman', 'size': 20}, fontsize=12)
+    ax1.set_title("Sensor_2", fontdict={'family': 'Times New Roman'}, fontsize=12)
     ax1.plot(x_axis, emg_filter_2, color="#B0BEC5", label="Raw", zorder=1)
     ax1.plot(
         x_axis, EMG_clean_2, color="#FFC107", label="Cleaned", zorder=1, linewidth=1.5
@@ -101,13 +108,13 @@ if __name__ == '__main__':
     A_2 = activationlevel(MVC_2, 0.35)
     # Plot absolute value and MVC of EMG.
     fig, (ax0, ax1) = plt.subplots(nrows=2, ncols=1, sharex=True)
-    ax1.set_xlabel("Time (seconds)", fontweight="bold", fontdict={'family': 'Times New Roman', 'size': 20}, fontsize=12)
+    ax1.set_xlabel("Time (seconds)", fontweight="bold", fontdict={'family': 'Times New Roman'}, fontsize=12)
     fig.suptitle("Absolute Value and MVC of EMG signals", fontweight="bold",
-                 fontdict={'family': 'Times New Roman', 'size': 20}, fontsize=16)
+                 fontdict={'family': 'Times New Roman'}, fontsize=16)
     plt.subplots_adjust(hspace=0.2)
     x_axis = np.linspace(0, EMG_abs_1.shape[0] / int(SAMPING_RATE / n), EMG_abs_1.shape[0])
     # Plot cleaned and raw EMG.
-    ax0.set_title("Sensor_1", fontdict={'family': 'Times New Roman', 'size': 20}, fontsize=12)
+    ax0.set_title("Sensor_1", fontdict={'family': 'Times New Roman'}, fontsize=12)
     ax0.plot(x_axis, EMG_abs_1, color="#B0BEC5", label="ABS", zorder=1)
     ax2 = ax0.twinx()
     ax2.plot(
@@ -116,7 +123,7 @@ if __name__ == '__main__':
     ax0.legend(loc="upper left", frameon=True, prop=legend_font)
     ax2.legend(loc="upper right", frameon=True, prop=legend_font)
 
-    ax1.set_title("Sensor_2", fontdict={'family': 'Times New Roman', 'size': 20}, fontsize=12)
+    ax1.set_title("Sensor_2", fontdict={'family': 'Times New Roman'}, fontsize=12)
     ax1.plot(x_axis, EMG_abs_2, color="#B0BEC5", label="ABS", zorder=1)
     ax3 = ax1.twinx()
     ax3.plot(
