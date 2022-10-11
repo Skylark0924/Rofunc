12edb74d1739d9668cc2ed6e26945ed173cb6524
skylark <l.syj@163.com>
2022-10-11 08:55:57 +0000

    Start RoboLab, add ilqr

---
+++ rofunc/utils/robolab/dynamics/id.py
@@ -0,0 +1,10 @@
+def id(urdf, force, export_joint="EE"):
+    export_joint_torque = ...
+    return export_joint_torque
+
+
+if __name__ == '__main__':
+    urdf = ...
+    force = []
+    joint_torque = id(urdf, force)
+    print(joint_torque)
