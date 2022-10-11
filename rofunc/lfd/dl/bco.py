2abc2bb57400fc12c1707bd620cf77e0aee5e331
ZhihaoAIRobotic <1155187971@link.cuhk.edu.hk>
2022-10-11 13:26:30 +0000

    add behavior cloning algorithm

---
+++ rofunc/lfd/dl/bco.py
@@ -0,0 +1,94 @@
+import torchvision
+import numpy as np
+import gym
+import torch
+import torch.nn as nn
+import torch.functional as F
+from matplotlib import pyplot as plt
+from matplotlib import style
+
+
+def data_process(states, actions=None, n=2, compare=1):
+    count = 0
+    index = []
+
+    if type(actions) != torch.Tensor:
+        ep, t, state_size = states.shape
+    else:
+        ep, t, state_size = states.shape
+        _, _, action_size = actions.shape
+
+    if type(actions) != torch.Tensor:
+        output_states = torch.zeros((ep * (t - n + 1), state_size * n), dtype=torch.float)
+    else:
+        output_states = torch.zeros((ep * (t - n + 1), state_size * n), dtype=torch.float)
+        output_actions = torch.zeros((ep * (t - n + 1), action_size), dtype=torch.float)
+
+    for i in range(ep):
+        for j in range(t - n + 1):
+            if (states[i, j] == -compare * torch.ones(state_size)).all() or (
+                    states[i, j + 1] == -compare * torch.ones(state_size)).all():
+                index.append([i, j])
+            else:
+                output_states[count] = states[i, j:j + n].view(-1)
+
+            if type(actions) != torch.Tensor:
+                count += 1
+                # do nothing
+            else:
+                output_actions[count] = actions[i, j]
+                count += 1
+
+    if type(actions) != torch.Tensor:
+        output_states = output_states[:count]
+        return output_states
+    else:
+        output_states = output_states[:count]
+        output_actions = output_actions[:count]
+        return output_states, output_actions
+
+
+def train_transition (training_set, state_space_size, model, n=2,   batch_size = 256, n_epoch = 50):
+    criterion = nn.L1Loss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    loss_list = []
+    for itr in range(n_epoch):
+        total_loss = 0
+        b=0
+        for batch in range (0,training_set.shape[0], batch_size):
+            data   = training_set  [batch : batch+batch_size , :n*state_space_size]
+            y      = training_set [batch : batch+batch_size, n*state_space_size:]
+            y_pred = model(data)
+            loss   = criterion(y_pred, y)
+            total_loss += loss.item()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            b += 1
+        print("[EPOCH]: %i, [LOSS]: %.6f" % (itr+1, total_loss/b))
+        loss_list.append(total_loss / training_set.shape[0])
+    return model
+
+
+def train_policy(training_set, state_space_size, policy, batch_size=256, n_epoch=50):
+    criterion = nn.L1Loss()
+    optimizer = torch.optim.Adam(policy.parameters(), lr=0.001)
+    loss_list = []
+    for itr in range(n_epoch):
+        total_loss = 0
+        b = 0
+        for batch in range(0, training_set.shape[0], batch_size):
+            data = training_set[batch: batch + batch_size, :state_space_size]
+            y = training_set[batch: batch + batch_size, state_space_size:]
+            y_pred = policy(data)
+            loss = criterion(y_pred, y)
+            total_loss += loss.item()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            b += 1
+        print("[EPOCH]: %i, [LOSS]: %.6f" % (itr + 1, total_loss / b))
+        loss_list.append(total_loss / training_set.shape[0])
+    return policy
+
+
