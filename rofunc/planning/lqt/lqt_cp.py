b79dfb3ad0aa43fd62c7693969d1c072d86b699b
skylark <l.syj@163.com>
2022-10-12 11:10:34 +0000

    hydra ok for lqt and lqt_cp

--- rofunc/planning/lqt/lqt_cp.py
+++ rofunc/planning/lqt/lqt_cp.py
@@ -1,73 +1,76 @@
 """
     Linear Quadratic tracker with control primitives applied on a via-point example
 """
-
 from typing import Dict, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
 import rofunc as rf
+from rofunc.config.get_config import *
 
 
-def define_control_primitive(param):
+def define_control_primitive(cfg):
     functions = {
         "PIECEWEISE": rf.primitive.build_phi_piecewise,
         "RBF": rf.primitive.build_phi_rbf,
         "BERNSTEIN": rf.primitive.build_phi_bernstein,
         "FOURIER": rf.primitive.build_phi_fourier
     }
-    phi = functions[param["basisName"]](param["nbData"] - 1, param["nbFct"])
-    PSI = np.kron(phi, np.identity(param["nbVarPos"]))
+    phi = functions[cfg.basisName](cfg.nbData - 1, cfg.nbFct)
+    PSI = np.kron(phi, np.identity(cfg.nbVarPos))
     return PSI, phi
 
 
-def set_dynamical_system(param: Dict):
-    nb_var = param["nb_var"]
+def set_dynamical_system(cfg: DictConfig):
+    nb_var = cfg.nbVar
     A = np.identity(nb_var)
-    if param["nbDeriv"] == 2:
-        A[:param["nbVarPos"], -param["nbVarPos"]:] = np.identity(param["nbVarPos"]) * param["dt"]
+    if cfg.nbDeriv == 2:
+        A[:cfg.nbVarPos, -cfg.nbVarPos:] = np.identity(cfg.nbVarPos) * cfg.dt
 
-    B = np.zeros((nb_var, param["nbVarPos"]))
-    derivatives = [param["dt"], param["dt"] ** 2 / 2][:param["nbDeriv"]]
-    for i in range(param["nbDeriv"]):
-        B[i * param["nbVarPos"]:(i + 1) * param["nbVarPos"]] = np.identity(param["nbVarPos"]) * derivatives[::-1][i]
+    B = np.zeros((nb_var, cfg.nbVarPos))
+    derivatives = [cfg.dt, cfg.dt ** 2 / 2][:cfg.nbDeriv]
+    for i in range(cfg.nbDeriv):
+        B[i * cfg.nbVarPos:(i + 1) * cfg.nbVarPos] = np.identity(cfg.nbVarPos) * derivatives[::-1][i]
 
     # Build Sx and Su transfer matrices
-    Su = np.zeros((nb_var * param["nbData"], param["nbVarPos"] * (param["nbData"] - 1)))  # It's maybe n-1 not sure
-    Sx = np.kron(np.ones((param["nbData"], 1)), np.eye(nb_var, nb_var))
+    Su = np.zeros((nb_var * cfg.nbData, cfg.nbVarPos * (cfg.nbData - 1)))  # It's maybe n-1 not sure
+    Sx = np.kron(np.ones((cfg.nbData, 1)), np.eye(nb_var, nb_var))
 
     M = B
-    for i in range(1, param["nbData"]):
-        Sx[i * nb_var:param["nbData"] * nb_var, :] = np.dot(Sx[i * nb_var:param["nbData"] * nb_var, :], A)
+    for i in range(1, cfg.nbData):
+        Sx[i * nb_var:cfg.nbData * nb_var, :] = np.dot(Sx[i * nb_var:cfg.nbData * nb_var, :], A)
         Su[nb_var * i:nb_var * i + M.shape[0], 0:M.shape[1]] = M
         M = np.hstack((np.dot(A, M), B))  # [0,nb_state_var-1]
     return Su, Sx
 
 
-def get_u_x(param: Dict, start_pose: np.ndarray, muQ: np.ndarray, Q: np.ndarray, R: np.ndarray, Su: np.ndarray,
+def get_u_x(cfg: DictConfig, start_pose: np.ndarray, muQ: np.ndarray, Q: np.ndarray, R: np.ndarray, Su: np.ndarray,
             Sx: np.ndarray, PSI: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-    x0 = start_pose.reshape((param['nb_var'], 1))
+    x0 = start_pose.reshape((cfg.nbVar, 1))
     w_hat = np.linalg.inv(PSI.T @ Su.T @ Q @ Su @ PSI + PSI.T @ R @ PSI) @ PSI.T @ Su.T @ Q @ (muQ - Sx @ x0)
     u_hat = PSI @ w_hat
-    x_hat = (Sx @ x0 + Su @ u_hat).reshape((-1, param["nb_var"]))
-    u_hat = u_hat.reshape((-1, param["nbVarPos"]))
+    x_hat = (Sx @ x0 + Su @ u_hat).reshape((-1, cfg.nbVar))
+    u_hat = u_hat.reshape((-1, cfg.nbVarPos))
     return u_hat, x_hat
 
 
-def uni_cp(param: Dict, data: np.ndarray):
+def uni_cp(data: np.ndarray, cfg: DictConfig = None):
     print('\033[1;32m--------{}--------\033[0m'.format('Planning smooth trajectory via LQT (control primitive)'))
-    data = data[:, :param['nbVarPos']]
 
-    start_pose = np.zeros((param['nb_var'],), dtype=np.float32)
-    start_pose[:param['nbVarPos']] = data[0]
+    cfg = get_config() if cfg is None else cfg
+
+    data = data[:, :cfg.nbVarPos]
+
+    start_pose = np.zeros((cfg.nbVar,), dtype=np.float32)
+    start_pose[:cfg.nbVarPos] = data[0]
 
     via_point_pose = data[1:]
-    param['nbPoints'] = len(via_point_pose)
+    cfg.nbPoints = len(via_point_pose)
 
-    via_point, muQ, Q, R, idx_slices, tl = rf.lqt.get_matrices(param, via_point_pose)
-    PSI, phi = define_control_primitive(param)
-    Su, Sx = set_dynamical_system(param)
-    u_hat, x_hat = get_u_x(param, start_pose, muQ, Q, R, Su, Sx, PSI)
+    via_point, muQ, Q, R, idx_slices, tl = rf.lqt.get_matrices(cfg, via_point_pose)
+    PSI, phi = define_control_primitive(cfg)
+    Su, Sx = set_dynamical_system(cfg)
+    u_hat, x_hat = get_u_x(cfg, start_pose, muQ, Q, R, Su, Sx, PSI)
 
     # vis(param, x_hat, u_hat, muQ, idx_slices, tl, phi)
     rf.lqt.plot_3d_uni([x_hat], muQ, idx_slices)
@@ -75,7 +78,7 @@ def uni_cp(param: Dict, data: np.ndarray):
     return u_hat, x_hat, muQ, idx_slices
 
 
-def vis(param, x_hat, u_hat, muQ, idx_slices, tl, phi):
+def vis(cfg, x_hat, u_hat, muQ, idx_slices, tl, phi):
     plt.figure()
 
     plt.title("2D Trajectory")
@@ -93,32 +96,32 @@ def vis(param, x_hat, u_hat, muQ, idx_slices, tl, phi):
 
     axs[0].plot(x_hat[:, 0], c='black')
     axs[0].set_ylabel("$x_1$")
-    axs[0].set_xticks([0, param["nbData"]])
+    axs[0].set_xticks([0, cfg.nbData])
     axs[0].set_xticklabels(["0", "T"])
     for t in tl:
         axs[0].scatter(t, x_hat[t, 0], c='blue')
 
     axs[1].plot(x_hat[:, 1], c='black')
     axs[1].set_ylabel("$x_2$")
-    axs[1].set_xticks([0, param["nbData"]])
+    axs[1].set_xticks([0, cfg.nbData])
     axs[1].set_xticklabels(["0", "T"])
     for t in tl:
         axs[1].scatter(t, x_hat[t, 1], c='blue')
 
     axs[2].plot(u_hat[:, 0], c='black')
     axs[2].set_ylabel("$u_1$")
-    axs[2].set_xticks([0, param["nbData"] - 1])
+    axs[2].set_xticks([0, cfg.nbData - 1])
     axs[2].set_xticklabels(["0", "T-1"])
 
     axs[3].plot(u_hat[:, 1], c='black')
     axs[3].set_ylabel("$u_2$")
-    axs[3].set_xticks([0, param["nbData"] - 1])
+    axs[3].set_xticks([0, cfg.nbData - 1])
     axs[3].set_xticklabels(["0", "T-1"])
 
     axs[4].set_ylabel("$\phi_k$")
-    axs[4].set_xticks([0, param["nbData"] - 1])
+    axs[4].set_xticks([0, cfg.nbData - 1])
     axs[4].set_xticklabels(["0", "T-1"])
-    for i in range(param["nbFct"]):
+    for i in range(cfg.nbFct):
         axs[4].plot(phi[:, i])
     axs[4].set_xlabel("$t$")
 
@@ -126,17 +129,6 @@ def vis(param, x_hat, u_hat, muQ, idx_slices, tl, phi):
 
 
 if __name__ == '__main__':
-    param = {
-        "nbData": 200,  # Number of data points
-        "nbVarPos": 7,  # Dimension of position data
-        "nbDeriv": 2,  # Number of static and dynamic features (2 -> [x,dx])
-        "nbFct": 9,  # Number of basis function
-        "basisName": "RBF",  # can be PIECEWEISE, RBF, BERNSTEIN, FOURIER
-        "dt": 1e-2,  # Time step duration
-        "rfactor": 1e-8  # Control cost
-    }
-    param["nb_var"] = param["nbVarPos"] * param["nbDeriv"]  # Dimension of state vector
-
     data_raw = np.load('/home/ubuntu/Data/2022_09_09_Taichi/rep3_r.npy')
     data = np.zeros((len(data_raw), 14))
     data[:, :7] = data_raw
@@ -146,4 +138,4 @@ if __name__ == '__main__':
 
     # via_points = np.array([[2, 5, 0, 0], [3, 1, 0, 0], [3, 6, 0, 0], [4, 2, 0, 0]])
 
-    uni_cp(param, via_points)
+    uni_cp(via_points)
--- rofunc/planning/lqt/lqt_fb.py
+++ rofunc/planning/lqt/lqt_fb.py
@@ -1,39 +1,38 @@
 """
     LQT computed in a recursive way (via-point example)
 """
-from typing import Dict
-
 import matplotlib.pyplot as plt
 import numpy as np
 import rofunc as rf
+from rofunc.config.get_config import *
 
 
-def get_matrices(param: Dict, data: np.ndarray):
-    param['nbPoints'] = len(data)
+def get_matrices(cfg: DictConfig, data: np.ndarray):
+    cfg.nbPoints = len(data)
 
-    R = np.eye(param["nbVarPos"]) * param["rfactor"]  # Control cost matrix
+    R = np.eye(cfg.nbVarPos) * cfg.rfactor  # Control cost matrix
 
     # Sparse reference with a set of via-points
-    tl = np.linspace(0, param["nbData"] - 1, param["nbPoints"] + 1)
+    tl = np.linspace(0, cfg.nbData - 1, cfg.nbPoints + 1)
     tl = np.rint(tl[1:])
 
     # Definition of augmented precision matrix Qa based on standard precision matrix Q0
-    Q0 = np.diag(np.hstack([np.ones(param["nbVarPos"]), np.zeros(param["nbVar"] - param["nbVarPos"])]))
+    Q0 = np.diag(np.hstack([np.ones(cfg.nbVarPos), np.zeros(cfg.nbVar - cfg.nbVarPos)]))
 
-    Q0_augmented = np.identity(param["nbVar"] + 1)
-    Q0_augmented[:param["nbVar"], :param["nbVar"]] = Q0
+    Q0_augmented = np.identity(cfg.nbVar + 1)
+    Q0_augmented[:cfg.nbVar, :cfg.nbVar] = Q0
 
-    Q = np.zeros([param["nbVar"] + 1, param["nbVar"] + 1, param["nbData"]])
-    for i in range(param["nbPoints"]):
+    Q = np.zeros([cfg.nbVar + 1, cfg.nbVar + 1, cfg.nbData])
+    for i in range(cfg.nbPoints):
         Q[:, :, int(tl[i])] = np.vstack([
-            np.hstack([np.identity(param["nbVar"]), np.zeros([param["nbVar"], 1])]),
+            np.hstack([np.identity(cfg.nbVar), np.zeros([cfg.nbVar, 1])]),
             np.hstack([-data[i, :], 1])]) @ Q0_augmented @ np.vstack([
-            np.hstack([np.identity(param["nbVar"]), -data[i, :].reshape([-1, 1])]),
-            np.hstack([np.zeros(param["nbVar"]), 1])])
+            np.hstack([np.identity(cfg.nbVar), -data[i, :].reshape([-1, 1])]),
+            np.hstack([np.zeros(cfg.nbVar), 1])])
     return Q, R, tl
 
 
-def set_dynamical_system(param: Dict):
+def set_dynamical_system(cfg: DictConfig):
     """
     Dynamical System settings (discrete version)
     Args:
@@ -42,23 +41,23 @@ def set_dynamical_system(param: Dict):
     Returns:
 
     """
-    A1d = np.zeros((param["nbDeriv"], param["nbDeriv"]))
-    for i in range(param["nbDeriv"]):
-        A1d += np.diag(np.ones((param["nbDeriv"] - i,)), i) * param["dt"] ** i / np.math.factorial(i)  # Discrete 1D
+    A1d = np.zeros((cfg.nbDeriv, cfg.nbDeriv))
+    for i in range(cfg.nbDeriv):
+        A1d += np.diag(np.ones((cfg.nbDeriv - i,)), i) * cfg.dt ** i / np.math.factorial(i)  # Discrete 1D
 
-    B1d = np.zeros((param["nbDeriv"], 1))
-    for i in range(0, param["nbDeriv"]):
-        B1d[param["nbDeriv"] - i - 1, :] = param["dt"] ** (i + 1) * 1 / np.math.factorial(i + 1)  # Discrete 1D
+    B1d = np.zeros((cfg.nbDeriv, 1))
+    for i in range(0, cfg.nbDeriv):
+        B1d[cfg.nbDeriv - i - 1, :] = cfg.dt ** (i + 1) * 1 / np.math.factorial(i + 1)  # Discrete 1D
 
-    A0 = np.kron(A1d, np.eye(param["nbVarPos"]))  # Discrete nD
-    B0 = np.kron(B1d, np.eye(param["nbVarPos"]))  # Discrete nD
+    A0 = np.kron(A1d, np.eye(cfg.nbVarPos))  # Discrete nD
+    B0 = np.kron(B1d, np.eye(cfg.nbVarPos))  # Discrete nD
     A = np.eye(A0.shape[0] + 1)  # Augmented A
     A[:A0.shape[0], :A0.shape[1]] = A0
-    B = np.vstack((B0, np.zeros((1, param["nbVarPos"]))))  # Augmented B
+    B = np.vstack((B0, np.zeros((1, cfg.nbVarPos))))  # Augmented B
     return A, B
 
 
-def get_u_x(param: Dict, state_noise: np.ndarray, P: np.ndarray, R: np.ndarray, A: np.ndarray, B: np.ndarray):
+def get_u_x(cfg: DictConfig, state_noise: np.ndarray, P: np.ndarray, R: np.ndarray, A: np.ndarray, B: np.ndarray):
     """
     Reproduction with only feedback (FB) on augmented state
     Args:
@@ -72,11 +71,11 @@ def get_u_x(param: Dict, state_noise: np.ndarray, P: np.ndarray, R: np.ndarray, 
     Returns:
 
     """
-    x_hat = np.zeros((param["nbVarX"], 2, param["nbData"]))
-    u_hat = np.zeros((param["nbVarPos"], 2, param["nbData"]))
+    x_hat = np.zeros((cfg.nbVarX, 2, cfg.nbData))
+    u_hat = np.zeros((cfg.nbVarPos, 2, cfg.nbData))
     for n in range(2):
-        x = np.hstack([np.zeros(param["nbVar"]), 1])
-        for t in range(param["nbData"]):
+        x = np.hstack([np.zeros(cfg.nbVar), 1])
+        for t in range(cfg.nbData):
             Z_bar = B.T @ P[:, :, t] @ B + R
             K = np.linalg.inv(Z_bar.T @ Z_bar) @ Z_bar.T @ B.T @ P[:, :, t] @ A  # Feedback gain
             u = -K @ x  # Acceleration command with FB on augmented state (resulting in feedback and feedforward terms)
@@ -96,7 +95,7 @@ def get_u_x(param: Dict, state_noise: np.ndarray, P: np.ndarray, R: np.ndarray, 
     return u_hat, x_hat
 
 
-def uni_fb(param: Dict, data: np.ndarray):
+def uni_fb(data: np.ndarray, cfg: DictConfig = None):
     """
     LQR with recursive computation and augmented state space
     Args:
@@ -106,19 +105,19 @@ def uni_fb(param: Dict, data: np.ndarray):
     Returns:
 
     """
-    Q, R, tl = get_matrices(param, data)
-    A, B = set_dynamical_system(param)
+    Q, R, tl = get_matrices(cfg, data)
+    A, B = set_dynamical_system(cfg)
 
-    state_noise = np.hstack((-1, -.2, 1, 0, 0, 0, 0, np.zeros(param["nbVarX"] - param["nbVarPos"])))
+    state_noise = np.hstack((-1, -.2, 1, 0, 0, 0, 0, np.zeros(cfg.nbVarX - cfg.nbVarPos)))
 
-    P = np.zeros((param["nbVarX"], param["nbVarX"], param["nbData"]))
+    P = np.zeros((cfg.nbVarX, cfg.nbVarX, cfg.nbData))
     P[:, :, -1] = Q[:, :, -1]
 
-    for t in range(param["nbData"] - 2, -1, -1):
+    for t in range(cfg.nbData - 2, -1, -1):
         P[:, :, t] = Q[:, :, t] - A.T @ (
                 P[:, :, t + 1] @ np.dot(B, np.linalg.pinv(B.T @ P[:, :, t + 1] @ B + R))
                 @ B.T @ P[:, :, t + 1] - P[:, :, t + 1]) @ A
-    u_hat, x_hat = get_u_x(param, state_noise, P, R, A, B)
+    u_hat, x_hat = get_u_x(cfg, state_noise, P, R, A, B)
     vis3d(data, x_hat)
     return u_hat, x_hat
 
@@ -147,16 +146,6 @@ def vis3d(data, x_hat):
 
 
 if __name__ == '__main__':
-    param = {
-        "nbData": 200,  # Number of datapoints
-        "nbVarPos": 7,  # Dimension of position data (here: x1,x2)
-        "nbDeriv": 2,  # Number of static and dynamic features (nbDeriv=2 for [x,dx] and u=ddx)
-        "dt": 1E-2,  # Time step duration
-        "rfactor": 1E-6,  # control cost in LQR
-    }
-    param["nbVar"] = param["nbVarPos"] * param["nbDeriv"]  # Dimension of state vector
-    param["nbVarX"] = param["nbVar"] + 1  # Augmented state space
-
     # 7-dim example
     via_points = np.zeros((3, 14))
     via_points[0, :7] = np.array([2, 5, 3, 0, 0, 0, 1])
@@ -166,4 +155,4 @@ if __name__ == '__main__':
     # 2-dim example
     # via_points = np.array([[2, 5, 0, 0], [3, 1, 0, 0]])
 
-    uni_fb(param, via_points)
+    uni_fb(via_points)
