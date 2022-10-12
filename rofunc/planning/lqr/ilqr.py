a84dfb859f3372ad839e2321dca380f778f8f6f0
skylark <l.syj@163.com>
2022-10-12 11:10:34 +0000

    hydra ok for lqt and lqt_cp

--- rofunc/planning/lqr/ilqr.py
+++ rofunc/planning/lqr/ilqr.py
@@ -5,6 +5,7 @@ from typing import Dict
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+from rofunc.config.get_config import *
 
 
 def logmap(f, f0):
@@ -14,62 +15,62 @@ def logmap(f, f0):
     return error
 
 
-def fkin(param, x):
-    L = np.tril(np.ones([param.nbVarX, param.nbVarX]))
+def fkin(cfg, x):
+    L = np.tril(np.ones([cfg.nbVarX, cfg.nbVarX]))
     f = np.vstack([
-        param.l @ np.cos(L @ x),
-        param.l @ np.sin(L @ x),
+        cfg.l @ np.cos(L @ x),
+        cfg.l @ np.sin(L @ x),
         np.mod(np.sum(x, 0) + np.pi, 2 * np.pi) - np.pi
     ])  # f1,f2,f3, where f3 is the orientation (single Euler angle for planar robot)
     return f
 
 
-def fk(param, x):
+def fk(cfg, x):
     """
     Forward kinematics for end-effector (in robot coordinate system)
     $f(x_t) = x_t$
     Args:
-        param:
+        cfg:
         x:
     Returns:
 
     """
     f = x
-    # L = np.tril(np.ones([param.nbVarX, param.nbVarX]))
+    # L = np.tril(np.ones([cfg.nbVarX, cfg.nbVarX]))
     # f = np.vstack([
-    #     param.l @ np.cos(L @ x),
-    #     param.l @ np.sin(L @ x),
+    #     cfg.l @ np.cos(L @ x),
+    #     cfg.l @ np.sin(L @ x),
     #     np.mod(np.sum(x, 0) + np.pi, 2 * np.pi) - np.pi
     # ])  # f1,f2,f3, where f3 is the orientation (single Euler angle for planar robot)
     return f
 
 
-def Jacobian(param, x):
+def Jacobian(cfg, x):
     """
     Jacobian with analytical computation (for single time step)
     $J(x_t)= \dfrac{\partial{f(x_t)}}{\partial{x_t}}$
     Args:
         x:
-        param:
+        cfg:
 
     Returns:
 
     """
     J = np.identity(len(x))
-    # L = np.tril(np.ones([param.nbVarX, param.nbVarX]))
+    # L = np.tril(np.ones([cfg.nbVarX, cfg.nbVarX]))
     # J = np.vstack([
-    #     -np.sin(L @ x).T @ np.diag(param.l) @ L,
-    #     np.cos(L @ x).T @ np.diag(param.l) @ L,
-    #     np.ones([1, param.nbVarX])
+    #     -np.sin(L @ x).T @ np.diag(cfg.l) @ L,
+    #     np.cos(L @ x).T @ np.diag(cfg.l) @ L,
+    #     np.ones([1, cfg.nbVarX])
     # ])
     return J
 
 
-def f_reach(param, robot_state, specific_robot=None):
+def f_reach(cfg, robot_state, specific_robot=None):
     """
     Error and Jacobian for a viapoints reaching task (in object coordinate system)
     Args:
-        param:
+        cfg:
         robot_state: joint state or Cartesian pose
     Returns:
 
@@ -77,73 +78,73 @@ def f_reach(param, robot_state, specific_robot=None):
     if specific_robot is not None:
         ee_pose = specific_robot.fk(robot_state)
     else:
-        ee_pose = fk(param, robot_state)
-    f = logmap(ee_pose, param.Mu)
-    J = np.zeros([param.nbPoints * param.nbVarF, param.nbPoints * param.nbVarX])
-    for t in range(param.nbPoints):
-        f[:2, t] = param.A[:, :, t].T @ f[:2, t]  # Object-oriented forward kinematics
-        Jtmp = Jacobian(robot_state[:, t], param)
-        Jtmp[:2] = param.A[:, :, t].T @ Jtmp[:2]  # Object centered Jacobian
+        ee_pose = fk(cfg, robot_state)
+    f = logmap(ee_pose, cfg.Mu)
+    J = np.zeros([cfg.nbPoints * cfg.nbVarF, cfg.nbPoints * cfg.nbVarX])
+    for t in range(cfg.nbPoints):
+        f[:2, t] = cfg.A[:, :, t].T @ f[:2, t]  # Object-oriented forward kinematics
+        Jtmp = Jacobian(robot_state[:, t], cfg)
+        Jtmp[:2] = cfg.A[:, :, t].T @ Jtmp[:2]  # Object centered Jacobian
 
-        if param.useBoundingBox:
+        if cfg.useBoundingBox:
             for i in range(2):
-                if abs(f[i, t]) < param.sz[i]:
+                if abs(f[i, t]) < cfg.sz[i]:
                     f[i, t] = 0
                     Jtmp[i] = 0
                 else:
-                    f[i, t] -= np.sign(f[i, t]) * param.sz[i]
+                    f[i, t] -= np.sign(f[i, t]) * cfg.sz[i]
 
-        J[t * param.nbVarF:(t + 1) * param.nbVarF, t * param.nbVarX:(t + 1) * param.nbVarX] = Jtmp
+        J[t * cfg.nbVarF:(t + 1) * cfg.nbVarF, t * cfg.nbVarX:(t + 1) * cfg.nbVarX] = Jtmp
     return f, J
 
 
-def get_matrices(param: Dict):
+def get_matrices(cfg: DictConfig):
     # Precision matrix
-    Q = np.identity(param.nbVarF * param.nbPoints)
+    Q = np.identity(cfg.nbVarF * cfg.nbPoints)
 
     # Control weight matrix
-    R = np.identity((param.nbData - 1) * param.nbVarU) * param.r
+    R = np.identity((cfg.nbData - 1) * cfg.nbVarU) * cfg.r
 
     # Time occurrence of viapoints
-    tl = np.linspace(0, param.nbData, param.nbPoints + 1)
+    tl = np.linspace(0, cfg.nbData, cfg.nbPoints + 1)
     tl = np.rint(tl[1:]).astype(np.int64) - 1
-    idx = np.array([i + np.arange(0, param.nbVarX, 1) for i in (tl * param.nbVarX)])
+    idx = np.array([i + np.arange(0, cfg.nbVarX, 1) for i in (tl * cfg.nbVarX)])
     return Q, R, idx, tl
 
 
-def set_dynamical_system(param: Dict):
+def set_dynamical_system(cfg: DictConfig):
     # Transfer matrices (for linear system as single integrator)
     Su0 = np.vstack([
-        np.zeros([param.nbVarX, param.nbVarX * (param.nbData - 1)]),
-        np.tril(np.kron(np.ones([param.nbData - 1, param.nbData - 1]), np.eye(param.nbVarX) * param.dt))
+        np.zeros([cfg.nbVarX, cfg.nbVarX * (cfg.nbData - 1)]),
+        np.tril(np.kron(np.ones([cfg.nbData - 1, cfg.nbData - 1]), np.eye(cfg.nbVarX) * cfg.dt))
     ])
-    Sx0 = np.kron(np.ones(param.nbData), np.identity(param.nbVarX)).T
+    Sx0 = np.kron(np.ones(cfg.nbData), np.identity(cfg.nbVarX)).T
     return Su0, Sx0
 
 
-def get_u_x(param: Dict, Q: np.ndarray, R: np.ndarray, Su0: np.ndarray,
+def get_u_x(cfg: DictConfig, Q: np.ndarray, R: np.ndarray, Su0: np.ndarray,
             Sx0: np.ndarray, idx: np.ndarray, tl: np.ndarray):
     Su = Su0[idx.flatten()]  # We remove the lines that are out of interest
 
-    u = np.zeros(param.nbVarU * (param.nbData - 1))  # Initial control command
+    u = np.zeros(cfg.nbVarU * (cfg.nbData - 1))  # Initial control command
     # x0 = np.array([3 * np.pi / 4, -np.pi / 2])  # Initial state
-    x0 = fkin(np.array([3 * np.pi / 4, -np.pi / 2, -np.pi / 4]), param).reshape((3, ))  # Initial state
+    x0 = fkin(np.array([3 * np.pi / 4, -np.pi / 2, -np.pi / 4]), cfg).reshape((3, ))  # Initial state
 
-    for i in range(param.nbIter):
+    for i in range(cfg.nbIter):
         x = Su0 @ u + Sx0 @ x0 # System evolution
-        x = x.reshape([param.nbVarX, param.nbData], order='F')
-        f, J = f_reach(x[:, tl], param)  # Residuals and Jacobians
+        x = x.reshape([cfg.nbVarX, cfg.nbData], order='F')
+        f, J = f_reach(x[:, tl], cfg)  # Residuals and Jacobians
         du = np.linalg.inv(Su.T @ J.T @ Q @ J @ Su + R) @ (
-                -Su.T @ J.T @ Q @ f.flatten('F') - u * param.r)  # Gauss-Newton update
+                -Su.T @ J.T @ Q @ f.flatten('F') - u * cfg.r)  # Gauss-Newton update
         # Estimate step size with backtracking line search method
         alpha = 2
-        cost0 = f.flatten('F').T @ Q @ f.flatten('F') + np.linalg.norm(u) ** 2 * param.r  # Cost
+        cost0 = f.flatten('F').T @ Q @ f.flatten('F') + np.linalg.norm(u) ** 2 * cfg.r  # Cost
         while True:
             utmp = u + du * alpha
             xtmp = Su0 @ utmp + Sx0 @ x0  # System evolution
-            xtmp = xtmp.reshape([param.nbVarX, param.nbData], order='F')
-            ftmp, _ = f_reach(xtmp[:, tl], param)  # Residuals
-            cost = ftmp.flatten('F').T @ Q @ ftmp.flatten('F') + np.linalg.norm(utmp) ** 2 * param.r  # Cost
+            xtmp = xtmp.reshape([cfg.nbVarX, cfg.nbData], order='F')
+            ftmp, _ = f_reach(xtmp[:, tl], cfg)  # Residuals
+            cost = ftmp.flatten('F').T @ Q @ ftmp.flatten('F') + np.linalg.norm(utmp) ** 2 * cfg.r  # Cost
             if cost < cost0 or alpha < 1e-3:
                 u = utmp
                 print("Iteration {}, cost: {}".format(i, cost))
@@ -154,23 +155,23 @@ def get_u_x(param: Dict, Q: np.ndarray, R: np.ndarray, Su0: np.ndarray,
     return u, x
 
 
-def uni_ilqr(param):
-    Q, R, idx, tl = get_matrices(param)
-    Su0, Sx0 = set_dynamical_system(param)
-    u, x = get_u_x(param, Q, R, Su0, Sx0, idx, tl)
-    vis(x, param, tl)
+def uni_ilqr(cfg):
+    Q, R, idx, tl = get_matrices(cfg)
+    Su0, Sx0 = set_dynamical_system(cfg)
+    u, x = get_u_x(cfg, Q, R, Su0, Sx0, idx, tl)
+    vis(x, cfg, tl)
 
 
-def vis(x, param, tl):
+def vis(x, cfg, tl):
     plt.figure()
     plt.axis('off')
     plt.gca().set_aspect('equal', adjustable='box')
 
     # Get points of interest
-    # f00 = fkin0(x[:, 0], param)
-    # f01 = fkin0(x[:, tl[0]], param)
-    # f02 = fkin0(x[:, tl[1]], param)
-    f = fk(x, param)
+    # f00 = fkin0(x[:, 0], cfg)
+    # f01 = fkin0(x[:, tl[0]], cfg)
+    # f02 = fkin0(x[:, tl[1]], cfg)
+    f = fk(x, cfg)
 
     # plt.plot(f00[0, :], f00[1, :], c='black', linewidth=5, alpha=.2)
     # plt.plot(f01[0, :], f01[1, :], c='black', linewidth=5, alpha=.4)
@@ -180,15 +181,15 @@ def vis(x, param, tl):
     # Plot bounding box or viapoints
     ax = plt.gca()
     color_map = ['deepskyblue', 'darkorange']
-    for t in range(param.nbPoints):
-        if param.useBoundingBox:
-            rect_origin = param.Mu[:2, t] - param.A[:, :, t] @ np.array(param.sz)
-            rect_orn = param.Mu[-1, t]
-            rect = patches.Rectangle(rect_origin, param.sz[0] * 2, param.sz[1] * 2, np.degrees(rect_orn),
+    for t in range(cfg.nbPoints):
+        if cfg.useBoundingBox:
+            rect_origin = cfg.Mu[:2, t] - cfg.A[:, :, t] @ np.array(cfg.sz)
+            rect_orn = cfg.Mu[-1, t]
+            rect = patches.Rectangle(rect_origin, cfg.sz[0] * 2, cfg.sz[1] * 2, np.degrees(rect_orn),
                                      color=color_map[t])
             ax.add_patch(rect)
         else:
-            plt.scatter(param.Mu[0, t], param.Mu[1, t], s=100, marker='X', c=color_map[t])
+            plt.scatter(cfg.Mu[0, t], cfg.Mu[1, t], s=100, marker='X', c=color_map[t])
 
     plt.show()
 
@@ -218,13 +219,13 @@ if __name__ == '__main__':
             self.useBoundingBox = True  # Consider bounding boxes for reaching cost
 
 
-    param = Param()
+    cfg = Param()
     # Object rotation matrices
-    for t in range(param.nbPoints):
-        orn_t = param.Mu[-1, t]
-        param.A[:, :, t] = np.asarray([
+    for t in range(cfg.nbPoints):
+        orn_t = cfg.Mu[-1, t]
+        cfg.A[:, :, t] = np.asarray([
             [np.cos(orn_t), -np.sin(orn_t)],
             [np.sin(orn_t), np.cos(orn_t)]
         ])
 
-    uni_ilqr(param)
+    uni_ilqr(cfg)
