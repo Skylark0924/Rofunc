e53b4c85e209ce62c3911d956e36102838050e0b
Chenzui LI <lichenzui1995@gmail.com>
2022-10-31 10:19:12 +0000

    update urdf

--- rofunc/utils/robolab/kinematics/fk.py
+++ rofunc/utils/robolab/kinematics/fk.py
@@ -1,10 +1,20 @@
+import ikpy.chain
+import ikpy.utils.plot as plot_utils
+import numpy as np
+import math
+
 def fk(urdf, joint_state, export_joint="EE"):
     export_joint_pose = ...
     return export_joint_pose
 
 
 if __name__ == '__main__':
-    urdf = ...
-    joint_state = []
-    pose = fk(urdf, joint_state)
-    print(pose)
+    my_robot = ikpy.chain.Chain.from_urdf_file("/home/ubuntu/Rofunc/rofunc/simulator/assets/urdf/franka_description/robots/franka_panda.urdf")
+    print(my_robot)
+
+
+
+    # urdf = ...
+    # joint_state = []
+    # pose = fk(urdf, joint_state)
+    # print(pose)
