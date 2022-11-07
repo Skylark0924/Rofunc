0d7d380ac0b33836de9fd143c2a232044df76e37
skylark <l.syj@163.com>
2022-11-07 14:31:07 +0000

    Add FrankaCabinet with IsaacGym and SKRL
    1. Solve the problem of finding the path of specific packages
    2. Connect Isaac gym with skrl framework

--- rofunc/config/custom_resolvers.py
+++ rofunc/config/custom_resolvers.py
@@ -2,6 +2,31 @@ from omegaconf import DictConfig, OmegaConf
 from functools import reduce
 import operator
 
-OmegaConf.register_new_resolver("add", lambda *x: int(sum(x)))
-OmegaConf.register_new_resolver("plus", lambda x, y: int(x) + int(y))
-OmegaConf.register_new_resolver("multi", lambda *x: int(reduce(operator.mul, x, 1)))
+try:
+    OmegaConf.register_new_resolver("add", lambda *x: int(sum(x)))
+except Exception as e:
+    pass
+try:
+    OmegaConf.register_new_resolver("plus", lambda x, y: int(x) + int(y))
+except Exception as e:
+    pass
+try:
+    OmegaConf.register_new_resolver("multi", lambda *x: int(reduce(operator.mul, x, 1)))
+except Exception as e:
+    pass
+try:
+    OmegaConf.register_new_resolver('eq', lambda x, y: x.lower() == y.lower())
+except Exception as e:
+    pass
+try:
+    OmegaConf.register_new_resolver('contains', lambda x, y: x.lower() in y.lower())
+except Exception as e:
+    pass
+try:
+    OmegaConf.register_new_resolver('if', lambda condition, a, b: a if condition else b)
+except Exception as e:
+    pass
+try:
+    OmegaConf.register_new_resolver('resolve_default', lambda default, arg: default if arg == '' else arg)
+except Exception as e:
+    pass
