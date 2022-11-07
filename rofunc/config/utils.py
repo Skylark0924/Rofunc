aa57653e279167f2da5bfbe57c96d8b2a2e9a37e
skylark <l.syj@163.com>
2022-11-07 14:31:07 +0000

    Add FrankaCabinet with IsaacGym and SKRL
    1. Solve the problem of finding the path of specific packages
    2. Connect Isaac gym with skrl framework

--- rofunc/config/utils.py
+++ rofunc/config/utils.py
@@ -3,14 +3,11 @@ from typing import Dict
 from omegaconf import DictConfig
 
 
-def omegaconf_to_dict(d: DictConfig) -> Dict:
+def omegaconf_to_dict(config: DictConfig) -> Dict:
     """
     Converts an omegaconf DictConfig to a python Dict, respecting variable interpolation.
     """
-    ret = {}
-    for k, v in d.items():
-        if isinstance(v, DictConfig):
-            ret[k] = omegaconf_to_dict(v)
-        else:
-            ret[k] = v
-    return ret
+    d = {}
+    for k, v in config.items():
+        d[k] = omegaconf_to_dict(v) if isinstance(v, DictConfig) else v
+    return d
