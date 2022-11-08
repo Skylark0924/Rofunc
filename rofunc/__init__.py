ae971ba900740de163340abc66913a072b09408b
skylark <l.syj@163.com>
2022-11-08 03:09:31 +0000

    Update RL algo system

--- rofunc/__init__.py
+++ rofunc/__init__.py
@@ -8,7 +8,7 @@ from .simulator import franka, dualfranka, curi, walker
 
 from .lfd.ml import tpgmm, gmr, tpgmr
 from .lfd.dl import bc, strans
-from .lfd.rl import ppo, dqn
+from .lfd.rl.online import dqn
 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
