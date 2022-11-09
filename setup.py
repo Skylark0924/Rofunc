c6e9dec55481e392861771547ffb0ad9424ac717
skylark <l.syj@163.com>
2022-11-09 08:38:51 +0000

    Add RL eval

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     url='https://github.com/Skylark0924/Rofunc',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['matplotlib', 'pandas', 'tqdm', 'pillow', 'pytransform3d', 'numpy==1.21.6', 'pynput', 
+    install_requires=['matplotlib', 'pandas', 'tqdm', 'pillow', 'pytransform3d', 'numpy', 'pynput',
                       'nestle', 'omegaconf', 'hydra-core', 'opencv-python', 'neurokit2', 'elegantrl', 'skrl'],
                       # 'pbdlib @ https://github.com/Skylark0924/Rofunc/releases/download/v0.0.0.7.1/pbdlib-0.1-py3-none-any.whl',
                       # 'isaacgym @ https://github.com/Skylark0924/Rofunc/releases/download/v0.0.0.9/isaacgym-1.0rc4-py3-none-any.whl',
