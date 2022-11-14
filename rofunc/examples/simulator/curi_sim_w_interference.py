b68cbdaabe21daaf392cbf53d4e17f8c9cd7d71d
Skylark <l.syj@163.com>
2022-11-14 15:36:20 +0800

    Reconstruct the curi sim and add the interference mode

--- rofunc/examples/simulator/curi_sim_w_interference.py
+++ rofunc/examples/simulator/curi_sim_w_interference.py
@@ -1,6 +1,6 @@
 """
-Tracking the trajectory with multiple joints by CURI
-============================================================
+Tracking the trajectory with interference by CURI
+=================================
 
 This example runs a Tai Chi demo bimanual trajectory by using CURI.
 """
@@ -15,4 +15,4 @@ args.use_gpu_pipeline = False
 traj_l = np.load(os.path.join(rf.file.get_rofunc_path(), 'data/taichi_1l.npy'))
 traj_r = np.load(os.path.join(rf.file.get_rofunc_path(), 'data/taichi_1r.npy'))
 rf.lqt.plot_3d_bi(traj_l, traj_r, ori=False)
-rf.curi.run_traj_multi_joints(args, traj=[traj_l, traj_r], update_freq=0.001)
+rf.curi.run_traj_multi_joints_with_interference(args, traj=[traj_l, traj_r], update_freq=0.001)
