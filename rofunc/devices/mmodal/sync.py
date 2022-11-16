ec7b577a667cd8b7defa15705500e5ba384db97f
ZhihaoAIRobotic <1155187971@link.cuhk.edu.hk>
2022-11-16 04:51:20 +0000

    add multi modal sync framework

---
+++ rofunc/devices/mmodal/sync.py
@@ -0,0 +1,30 @@
+import pandas
+import numpy
+
+
+# 1. get the time of each device
+def get_optitrack_time():
+    pass
+
+
+def get_xsens_time():
+    pass
+
+
+def get_zed_time():
+    pass
+
+
+# 2. get the time offset between each device and the master device
+def get_time_offset():
+    pass
+
+
+def get_time_offset_master():
+    pass
+
+
+# 3.time sync
+def data_sync():
+    pass
+
