d43b6de7f443e3eab9edd60ebce255a6cf5f10c3
skylark <l.syj@163.com>
2022-11-17 12:50:12 +0800

    Add humanoid

--- rofunc/examples/learning/Humanoid_skrl.py
+++ rofunc/examples/learning/Humanoid_skrl.py
@@ -1,119 +1,148 @@
 """
-"This" is my example-script
+Humanoid
 ===========================
 
 This example doesn't do much, it just makes a simple plot
 """
 
-import isaacgym
+import argparse
+import sys
 
-import torch
-import torch.nn as nn
+from rofunc.config.get_config import get_config
+from rofunc.config.utils import omegaconf_to_dict
+from rofunc.examples.learning.base_skrl import set_cfg_ppo, set_models_ppo
+from rofunc.examples.learning.tasks import task_map
+from rofunc.data.models import model_zoo
+from rofunc.utils.logger.beauty_logger import beauty_print
 
-# Import the skrl components to build the RL system
-from skrl.models.torch import Model, GaussianMixin, DeterministicMixin
-from skrl.memories.torch import RandomMemory
-from skrl.agents.torch.ppo import PPO, PPO_DEFAULT_CONFIG
-from skrl.resources.schedulers.torch import KLAdaptiveRL
-from skrl.resources.preprocessors.torch import RunningStandardScaler
-from skrl.trainers.torch import SequentialTrainer
+from hydra._internal.utils import get_args_parser
+from skrl.agents.torch.ppo import PPO
+from skrl.agents.torch.sac import SAC
+from skrl.agents.torch.td3 import TD3
+from skrl.agents.torch.amp import AMP
 from skrl.envs.torch import wrap_env
-from skrl.envs.torch import load_isaacgym_env_preview4
+from skrl.memories.torch import RandomMemory
+# Import the skrl components to build the RL system
+from skrl.trainers.torch import SequentialTrainer
 from skrl.utils import set_seed
 
-# set the seed for reproducibility
-set_seed(42)
+
+def setup(custom_args, task_name, eval_mode=False):
+    # set the seed for reproducibility
+    set_seed(42)
+
+    # get config
+    sys.argv.append("task={}".format(task_name))
+    sys.argv.append("sim_device={}".format(custom_args.sim_device))
+    sys.argv.append("rl_device={}".format(custom_args.rl_device))
+    sys.argv.append("graphics_device_id={}".format(custom_args.graphics_device_id))
+    args = get_args_parser().parse_args()
+    cfg = get_config('./learning/rl', 'config', args=args)
+    cfg_dict = omegaconf_to_dict(cfg.task)
+
+    if eval_mode:
+        cfg_dict['env']['numEnvs'] = 16
+
+    env = task_map[task_name](cfg=cfg_dict,
+                              rl_device=cfg.rl_device,
+                              sim_device=cfg.sim_device,
+                              graphics_device_id=cfg.graphics_device_id,
+                              headless=cfg.headless,
+                              virtual_screen_capture=cfg.capture_video,  # TODO: check
+                              force_render=cfg.force_render)
+    env = wrap_env(env)
+
+    device = env.device
+
+    # Instantiate a RandomMemory as rollout buffer (any memory can be used for this)
+    memory = RandomMemory(memory_size=16, num_envs=env.num_envs, device=device)
+
+    if custom_args.agent == "ppo":
+        models_ppo = set_models_ppo(cfg, env, device)
+        cfg_ppo = set_cfg_ppo(cfg, env, device, eval_mode)
+        agent = PPO(models=models_ppo,
+                    memory=memory,
+                    cfg=cfg_ppo,
+                    observation_space=env.observation_space,
+                    action_space=env.action_space,
+                    device=device)
+    elif custom_args.agent == "sac":
+        models_sac = set_models_sac(cfg, env, device)
+        cfg_sac = set_cfg_sac(cfg, env, device, eval_mode)
+        agent = SAC(models=models_sac,
+                    memory=memory,
+                    cfg=cfg_sac,
+                    observation_space=env.observation_space,
+                    action_space=env.action_space,
+                    device=device)
+    elif custom_args.agent == "td3":
+        models_td3 = set_models_td3(cfg, env, device)
+        cfg_td3 = set_cfg_td3(cfg, env, device, eval_mode)
+        agent = TD3(models=models_td3,
+                    memory=memory,
+                    cfg=cfg_td3,
+                    observation_space=env.observation_space,
+                    action_space=env.action_space,
+                    device=device)
+    elif custom_args.agent == "amp":
+        models_amp = set_models_amp(cfg, env, device)
+        cfg_amp = set_cfg_amp(cfg, env, device, eval_mode)
+        agent = AMP(models=models_amp,
+                    memory=memory,
+                    cfg=cfg_amp,
+                    observation_space=env.observation_space,
+                    action_space=env.action_space,
+                    device=device)
+    else:
+        raise ValueError("Agent not supported")
+
+    return env, agent
 
 
-# Define the shared model (stochastic and deterministic models) for the agent using mixins.
-class Shared(GaussianMixin, DeterministicMixin, Model):
-    def __init__(self, observation_space, action_space, device, clip_actions=False,
-                 clip_log_std=True, min_log_std=-20, max_log_std=2, reduction="sum"):
-        Model.__init__(self, observation_space, action_space, device)
-        GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std, reduction)
-        DeterministicMixin.__init__(self, clip_actions)
+def train(custom_args, task_name):
+    beauty_print("Start training")
 
-        self.net = nn.Sequential(nn.Linear(self.num_observations, 400),
-                                 nn.ELU(),
-                                 nn.Linear(400, 200),
-                                 nn.ELU(),
-                                 nn.Linear(200, 100),
-                                 nn.ELU())
+    env, agent = setup(custom_args, task_name)
 
-        self.mean_layer = nn.Linear(100, self.num_actions)
-        self.log_std_parameter = nn.Parameter(torch.zeros(self.num_actions))
+    # Configure and instantiate the RL trainer
+    cfg_trainer = {"timesteps": 40000, "headless": True}
+    trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
 
-        self.value_layer = nn.Linear(100, 1)
-
-    def act(self, states, taken_actions, role):
-        if role == "policy":
-            return GaussianMixin.act(self, states, taken_actions, role)
-        elif role == "value":
-            return DeterministicMixin.act(self, states, taken_actions, role)
-
-    def compute(self, states, taken_actions, role):
-        if role == "policy":
-            return self.mean_layer(self.net(states)), self.log_std_parameter
-        elif role == "value":
-            return self.value_layer(self.net(states))
+    # start training
+    trainer.train()
 
 
-# Load and wrap the Isaac Gym environment
-env = load_isaacgym_env_preview4(task_name="Humanoid")  # preview 3 and 4 use the same loader
-env = wrap_env(env)
+def eval(custom_args, task_name, ckpt_path=None):
+    beauty_print("Start evaluating")
 
-device = env.device
+    env, agent = setup(custom_args, task_name, eval_mode=True)
 
-# Instantiate a RandomMemory as rollout buffer (any memory can be used for this)
-memory = RandomMemory(memory_size=32, num_envs=env.num_envs, device=device)
+    # load checkpoint (agent)
+    if ckpt_path is None:
+        ckpt_path = model_zoo(name="CURICabinetPPO_right_arm.pt")
+    agent.load(ckpt_path)
 
-# Instantiate the agent's models (function approximators).
-# PPO requires 2 models, visit its documentation for more details
-# https://skrl.readthedocs.io/en/latest/modules/skrl.agents.ppo.html#spaces-and-models
-models_ppo = {}
-models_ppo["policy"] = Shared(env.observation_space, env.action_space, device)
-models_ppo["value"] = models_ppo["policy"]  # same instance: shared model
+    # Configure and instantiate the RL trainer
+    cfg_trainer = {"timesteps": 1600, "headless": True}
+    trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
 
-# Configure and instantiate the agent.
-# Only modify some of the default configuration, visit its documentation to see all the options
-# https://skrl.readthedocs.io/en/latest/modules/skrl.agents.ppo.html#configuration-and-hyperparameters
-cfg_ppo = PPO_DEFAULT_CONFIG.copy()
-cfg_ppo["rollouts"] = 32  # memory_size
-cfg_ppo["learning_epochs"] = 5
-cfg_ppo["mini_batches"] = 4  # 32 * 4096 / 32768
-cfg_ppo["discount_factor"] = 0.99
-cfg_ppo["lambda"] = 0.95
-cfg_ppo["learning_rate"] = 5e-4
-cfg_ppo["learning_rate_scheduler"] = KLAdaptiveRL
-cfg_ppo["learning_rate_scheduler_kwargs"] = {"kl_threshold": 0.008}
-cfg_ppo["random_timesteps"] = 0
-cfg_ppo["learning_starts"] = 0
-cfg_ppo["grad_norm_clip"] = 1.0
-cfg_ppo["ratio_clip"] = 0.2
-cfg_ppo["value_clip"] = 0.2
-cfg_ppo["clip_predicted_values"] = True
-cfg_ppo["entropy_loss_scale"] = 0.0
-cfg_ppo["value_loss_scale"] = 2.0
-cfg_ppo["kl_threshold"] = 0
-cfg_ppo["rewards_shaper"] = lambda rewards, timestep, timesteps: rewards * 0.01
-cfg_ppo["state_preprocessor"] = RunningStandardScaler
-cfg_ppo["state_preprocessor_kwargs"] = {"size": env.observation_space, "device": device}
-cfg_ppo["value_preprocessor"] = RunningStandardScaler
-cfg_ppo["value_preprocessor_kwargs"] = {"size": 1, "device": device}
-# logging to TensorBoard and write checkpoints each 160 and 1600 timesteps respectively
-cfg_ppo["experiment"]["write_interval"] = 160
-cfg_ppo["experiment"]["checkpoint_interval"] = 1600
+    # evaluate the agent
+    trainer.eval()
 
-agent = PPO(models=models_ppo,
-            memory=memory,
-            cfg=cfg_ppo,
-            observation_space=env.observation_space,
-            action_space=env.action_space,
-            device=device)
 
-# Configure and instantiate the RL trainer
-cfg_trainer = {"timesteps": 32000, "headless": True}
-trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--agent", type=str, default="ppo")
+    parser.add_argument("--sim_device", type=str, default="cuda:1")
+    parser.add_argument("--rl_device", type=str, default="cuda:1")
+    parser.add_argument("--graphics_device_id", type=int, default=1)
+    parser.add_argument("--train", action="store_false", help="turn to train mode while adding this argument")
+    custom_args = parser.parse_args()
 
-# start training
-trainer.train()
+    task_name = "Humanoid"
+
+    if custom_args.train:
+        train(custom_args, task_name)
+    else:
+        ckpt_path = "/home/ubuntu/Github/Knowledge-Universe/Robotics/Roadmap-for-robot-science/rofunc/examples/learning/runs/CURICabinetBimanualPPO_22-11-16_17-46-07-677096/checkpoints/best_agent.pt"
+        eval(custom_args, task_name, ckpt_path=ckpt_path)
