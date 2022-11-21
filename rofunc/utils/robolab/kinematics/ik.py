bae5816e183164fbbad36eecc24ec24d0760d5b1
Lee950507 <lichenzui1995@gmail.com>
2022-11-22 01:50:08 +0800

    ik_pinocchio_test

--- rofunc/utils/robolab/kinematics/ik.py
+++ rofunc/utils/robolab/kinematics/ik.py
@@ -1,17 +1,66 @@
-import ikpy.chain
+from __future__ import print_function
+
 import numpy as np
-import ikpy.utils.plot as plot_utils
+from numpy.linalg import norm, solve
+from sys import argv
+from os.path import dirname, join, abspath
 
-def ik(urdf, pose, export_joint="EE"):
-    export_joint_state = ...
-    return export_joint_state
+import pinocchio
 
+# model = pinocchio.buildSampleModelManipulator()
+# data = model.createData()
 
-if __name__ == '__main__':
-    robot = ikpy.chain.Chain.from_urdf_file("/home/lee/Rofunc/rofunc/simulator/assets/urdf/curi/urdf/curi.urdf")
-    target_position = [0.1, 0.1, 0.5]
-    print("The angles of each joints are : ", robot.inverse_kinematics(target_position))
-    # urdf = ...
-    # pose = []
-    # joint_state = ik(urdf, pose)
-    # print(joint_state)
+# Load the urdf model
+# model = pinocchio.buildModelFromUrdf("/home/lee/Rofunc/rofunc/utils/robolab/franka_description/robots/franka_panda.urdf")
+model = pinocchio.buildModelFromUrdf("/home/lee/Rofunc/rofunc/simulator/assets/urdf/curi/urdf/curi_pinocchio_test.urdf")
+print('model name: ' + model.name)
+
+# Create data required by the algorithms
+data = model.createData()
+
+# Sample a random configuration
+q = pinocchio.randomConfiguration(model)
+print('q: %s' % q.T)
+
+# Perform the forward kinematics over the kinematic tree
+pinocchio.forwardKinematics(model, data, q)
+
+# Print out the placement of each joint of the kinematic tree
+for name, oMi in zip(model.names, data.oMi):
+    print(("{:<24} : {: .2f} {: .2f} {: .2f}"
+           .format(name, *oMi.translation.T.flat)))
+
+JOINT_ID = 9
+oMdes = pinocchio.SE3(np.eye(3), np.array([-0.2, 0.2, 0.1]))
+
+q = pinocchio.neutral(model)
+eps = 1e-4
+IT_MAX = 1000
+DT = 1e-1
+damp = 1e-12
+
+i = 0
+while True:
+    pinocchio.forwardKinematics(model, data, q)
+    dMi = oMdes.actInv(data.oMi[JOINT_ID])
+    err = pinocchio.log(dMi).vector
+    if norm(err) < eps:
+        success = True
+        break
+    if i >= IT_MAX:
+        success = False
+        break
+    J = pinocchio.computeJointJacobian(model, data, q, JOINT_ID)
+    v = - J.T.dot(solve(J.dot(J.T) + damp * np.eye(6), err))
+    q = pinocchio.integrate(model, q, v * DT)
+    if not i % 10:
+        print('%d: error = %s' % (i, err.T))
+    i += 1
+
+if success:
+    print("Convergence achieved!")
+else:
+    print("\nWarning: the iterative algorithm has not reached convergence to the desired precision")
+
+print('\nresult: %s' % q.flatten().tolist())
+print('\nfinal error: %s' % err.T)
