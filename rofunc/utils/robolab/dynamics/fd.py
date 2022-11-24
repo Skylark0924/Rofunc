9512042e29d003ca7f41e47aa0264ba631920ee7
skylark <l.syj@163.com>
2022-11-24 18:03:04 +0800

    fix the bug

--- rofunc/utils/robolab/dynamics/fd.py
+++ rofunc/utils/robolab/dynamics/fd.py
@@ -19,24 +19,3 @@ def fd(model, data, *args):
             return pinocchio.aba(model, data, q, v, tau)
 
     return pinocchio.aba(model, data, *args)
-
-
-if __name__ == '__main__':
-    model = pinocchio.buildModelFromUrdf(
-        "/home/ubuntu/Rofunc/rofunc/simulator/assets/urdf/curi/urdf/curi_pinocchio_test.urdf")
-    print('model name: ' + model.name)
-    data = model.createData()
-
-    q = pinocchio.randomConfiguration(model)
-    q = pinocchio.normalize(model, q)
-    v = np.matrix(np.random.rand(model.nv, 1))
-    tau = np.matripinocchio.abax(np.random.rand(model.nv, 1))
-
-    ddq = fd(model, data, q, v, tau)
-    print(ddq)
-
-    # pinocchio.computeABADerivatives(model, data, q, v, tau)
-    #
-    # ddq_dq = data.ddq_dq  # Derivatives of the FD w.r.t. the joint config vector
-    # ddq_dv = data.ddq_dv  # Derivatives of the FD w.r.t. the joint velocity vector
-    # ddq_dtau = data.Minv  # Derivatives of the FD w.r.t. the joint acceleration vector
