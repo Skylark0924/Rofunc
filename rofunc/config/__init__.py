8f30e1d27f785b6b9a09139ad38d5069b2a2280d
skylark <l.syj@163.com>
2022-11-30 00:02:25 +0800

    some failed and trash code for integrating StableBaseline3 with IsaacGym

--- rofunc/config/__init__.py
+++ rofunc/config/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import
 
 from . import custom_resolvers
-from .utils import get_config, omegaconf_to_dict, dict_to_omegaconf
+from .utils import get_config, print_config, omegaconf_to_dict, dict_to_omegaconf
