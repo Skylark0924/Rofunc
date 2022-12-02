c4670146070bcf8d8960bb85f977b5220ada4c0d
Junjia Liu <l.syj@163.com>
2022-12-02 18:37:22 +0800

    Rectified emg record

--- rofunc/examples/data_collection/emg_record.py
+++ rofunc/examples/data_collection/emg_record.py
@@ -14,9 +14,9 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     '-a', '--addr',
     dest='host',
-    default='10.13.166.60',
+    default='10.13.90.12',
     help="IP address of the machine running TCU. Default is localhost.")
 args = parser.parse_args()
 
 # For instance, 4 channels, 2000 samples per read and 10 batches are chosen.
-rf.emg.record(args.host, 4, 2000, 10)
+rf.emg.record(args.host, 4, 2000, 10, save_path="/home/skylark/Data/emg_record")
--- rofunc/examples/data_collection/zed_export.py
+++ rofunc/examples/data_collection/zed_export.py
@@ -8,7 +8,7 @@ This example shows how to export a SVO file to a video or image sequence.
 import rofunc as rf
 
 # Export a single svo file
-rf.zed.export('/home/ubuntu/Data/06_24/Video/20220624_1649/38709363.svo', 2)
+rf.zed.export('/home/skylark/Data/zed_record/HD1080_SN36605831_18-03-10.svo', 0)
 
 # Export a batch of SVO files in a directory
-rf.zed.export_batch('/home/ubuntu/Data/06_24/Video/20220624_1649', core_num=20)
+# rf.zed.export_batch('/home/ubuntu/Data/06_24/Video/20220624_1649', core_num=20)
