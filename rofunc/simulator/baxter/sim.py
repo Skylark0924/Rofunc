cf3ffb367c40638c83c87ef97b5f025f577d42c1
skylark <l.syj@163.com>
2022-12-06 00:22:58 +0800

    Thoroughly refactored the code of simulator

--- rofunc/simulator/baxter/sim.py
+++ rofunc/simulator/baxter/sim.py
@@ -15,7 +15,7 @@ def show(args, asset_root=None):
     from rofunc.simulator.base.base_sim import init_sim, init_env
     from rofunc.utils.logger.beauty_logger import beauty_print
 
-    beauty_print("Show the Ubtech Walker in the interactive mode", 1)
+    beauty_print("Show the CURI mini simulator in the interactive mode", 1)
 
     # Initial gym and sim
     gym, sim_params, sim, viewer = init_sim(args)
@@ -25,9 +25,9 @@ def show(args, asset_root=None):
         import site
         pip_root_path = site.getsitepackages()[0]
         asset_root = os.path.join(pip_root_path, "rofunc/simulator/assets")
-    asset_file = "urdf/walker/urdf/walker.urdf"
-    init_env(gym, sim, asset_root, asset_file, num_envs=5, spacing=3.0, fix_base_link=True,
-             flip_visual_attachments=False, init_pose_vec=(0, 2.0, 0.0))
+    asset_file = "urdf/baxter/robot.xml"
+    init_env(gym, sim, asset_root, asset_file, num_envs=5, spacing=3.0, fix_base_link=False,
+             flip_visual_attachments=False, init_pose_vec=(0, 1.0, 0.0))
 
     while not gym.query_viewer_has_closed(viewer):
         # Step the physics
@@ -38,3 +38,12 @@ def show(args, asset_root=None):
         gym.step_graphics(sim)
         gym.draw_viewer(viewer, sim, False)
         gym.sync_frame_time(sim)
+
+
+if __name__ == '__main__':
+    from isaacgym import gymutil
+
+    args = gymutil.parse_arguments()
+    args.use_gpu_pipeline = False
+
+    show(args)
