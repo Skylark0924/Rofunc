5df86e715ceda8271a87889988f9dd06ceb71ffa
Chenzui LI <lichenzui1995@gmail.com>
2022-12-09 10:26:27 +0000

    update

--- rofunc/data/BoB/visual.py
+++ rofunc/data/BoB/visual.py
@@ -1,12 +1,16 @@
 import pandas as pd
+import os
+import rofunc as rf
 import csv
 import numpy as np
 
-with open("/home/ubuntu/BoB_data/2022-12-03/BoB data/bench_press/all_muscle_forces.csv", 'r') as x:
-    sample_data = list(csv.reader(x, delimiter=","))
-data = np.array(sample_data[1:])
-a = data[1]
+demo_path = os.path.join(input_path, demo_csv)
+out_file_path = os.path.join(out_path, demo_csv)
+rf.utils.delete_lines(demo_path, out_file_path, 14)
+data = pd.read_csv("/home/ubuntu/BoB_data/2022-12-03/BoB data/bench_press/all_muscle_forces.csv", index_col=0)
+# data = np.array(data)
 
 
 
-print(list)
+
+print(data)
