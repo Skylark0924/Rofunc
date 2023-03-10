5b05c87b1e9e566103c07ba8b925e63ed793762b
skylark <l.syj@163.com>
2023-03-10 16:36:14 +0800

    Reformat all LQT

--- rofunc/planning/lqt/__init__.py
+++ rofunc/planning/lqt/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 from .lqt import *
+from .lqt_cp import *
+from .lqt_fb import *
+from .lqt_cp_dmp import *
 from .visualize import *
-from .lqt_cp import uni_cp, define_control_primitive
-from .lqt_fb import uni_fb
-from .lqt_cp_dmp import uni_cp_dmp
--- rofunc/planning/lqt/lqt_cp.py
+++ rofunc/planning/lqt/lqt_cp.py
@@ -10,118 +10,114 @@ import numpy as np
 from omegaconf import DictConfig
 
 import rofunc as rf
-from rofunc.config.utils import get_config
+from rofunc.planning.lqt.lqt import LQT
+from rofunc.utils.logger.beauty_logger import beauty_print
 
 
-def define_control_primitive(cfg):
-    functions = {
-        "PIECEWISE": rf.primitive.build_phi_piecewise,
-        "RBF": rf.primitive.build_phi_rbf,
-        "BERNSTEIN": rf.primitive.build_phi_bernstein,
-        "FOURIER": rf.primitive.build_phi_fourier
-    }
-    phi = functions[cfg.basisName](cfg.nbData - 1, cfg.nbFct)
-    PSI = np.kron(phi, np.identity(cfg.nbVarPos))
-    return PSI, phi
+class LQTCP(LQT):
+    def __init__(self, all_points, cfg: DictConfig = None):
+        super().__init__(all_points, cfg)
 
+    def define_control_primitive(self):
+        functions = {
+            "PIECEWISE": rf.primitive.build_phi_piecewise,
+            "RBF": rf.primitive.build_phi_rbf,
+            "BERNSTEIN": rf.primitive.build_phi_bernstein,
+            "FOURIER": rf.primitive.build_phi_fourier
+        }
+        phi = functions[self.cfg.basisName](self.cfg.nbData - 1, self.cfg.nbFct)
+        PSI = np.kron(phi, np.identity(self.cfg.nbVarPos))
+        return PSI, phi
 
-def set_dynamical_system(cfg: DictConfig):
-    A = np.identity(cfg.nbVar)
-    if cfg.nbDeriv == 2:
-        A[:cfg.nbVarPos, -cfg.nbVarPos:] = np.identity(cfg.nbVarPos) * cfg.dt
+    def set_dynamical_system(self):
+        A = np.identity(self.cfg.nbVar)
+        if self.cfg.nbDeriv == 2:
+            A[:self.cfg.nbVarPos, -self.cfg.nbVarPos:] = np.identity(self.cfg.nbVarPos) * self.cfg.dt
 
-    B = np.zeros((cfg.nbVar, cfg.nbVarPos))
-    derivatives = [cfg.dt, cfg.dt ** 2 / 2][:cfg.nbDeriv]
-    for i in range(cfg.nbDeriv):
-        B[i * cfg.nbVarPos:(i + 1) * cfg.nbVarPos] = np.identity(cfg.nbVarPos) * derivatives[::-1][i]
+        B = np.zeros((self.cfg.nbVar, self.cfg.nbVarPos))
+        derivatives = [self.cfg.dt, self.cfg.dt ** 2 / 2][:self.cfg.nbDeriv]
+        for i in range(self.cfg.nbDeriv):
+            B[i * self.cfg.nbVarPos:(i + 1) * self.cfg.nbVarPos] = np.identity(self.cfg.nbVarPos) * derivatives[::-1][i]
 
-    # Build Sx and Su transfer matrices
-    Su = np.zeros((cfg.nbVar * cfg.nbData, cfg.nbVarPos * (cfg.nbData - 1)))  # It's maybe n-1 not sure
-    Sx = np.kron(np.ones((cfg.nbData, 1)), np.eye(cfg.nbVar, cfg.nbVar))
+        # Build Sx and Su transfer matrices
+        Su = np.zeros(
+            (self.cfg.nbVar * self.cfg.nbData, self.cfg.nbVarPos * (self.cfg.nbData - 1)))  # It's maybe n-1 not sure
+        Sx = np.kron(np.ones((self.cfg.nbData, 1)), np.eye(self.cfg.nbVar, self.cfg.nbVar))
 
-    M = B
-    for i in range(1, cfg.nbData):
-        Sx[i * cfg.nbVar:cfg.nbData * cfg.nbVar, :] = np.dot(Sx[i * cfg.nbVar:cfg.nbData * cfg.nbVar, :], A)
-        Su[cfg.nbVar * i:cfg.nbVar * i + M.shape[0], 0:M.shape[1]] = M
-        M = np.hstack((np.dot(A, M), B))  # [0,nb_state_var-1]
-    return Su, Sx
+        M = B
+        for i in range(1, self.cfg.nbData):
+            Sx[i * self.cfg.nbVar:self.cfg.nbData * self.cfg.nbVar, :] = np.dot(
+                Sx[i * self.cfg.nbVar:self.cfg.nbData * self.cfg.nbVar, :], A)
+            Su[self.cfg.nbVar * i:self.cfg.nbVar * i + M.shape[0], 0:M.shape[1]] = M
+            M = np.hstack((np.dot(A, M), B))  # [0,nb_state_var-1]
+        return Su, Sx
 
+    def get_u_x(self, mu: np.ndarray, Q: np.ndarray, R: np.ndarray, Su: np.ndarray, Sx: np.ndarray, PSI: np.ndarray) -> \
+            Tuple[np.ndarray, np.ndarray]:
+        x0 = self.start_point.reshape((self.cfg.nbVar, 1))
+        w_hat = np.linalg.inv(PSI.T @ Su.T @ Q @ Su @ PSI + PSI.T @ R @ PSI) @ PSI.T @ Su.T @ Q @ (mu - Sx @ x0)
+        u_hat = PSI @ w_hat
+        x_hat = (Sx @ x0 + Su @ u_hat).reshape((-1, self.cfg.nbVar))
+        u_hat = u_hat.reshape((-1, self.cfg.nbVarPos))
+        return u_hat, x_hat
 
-def get_u_x(cfg: DictConfig, start_pose: np.ndarray, muQ: np.ndarray, Q: np.ndarray, R: np.ndarray, Su: np.ndarray,
-            Sx: np.ndarray, PSI: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-    x0 = start_pose.reshape((cfg.nbVar, 1))
-    w_hat = np.linalg.inv(PSI.T @ Su.T @ Q @ Su @ PSI + PSI.T @ R @ PSI) @ PSI.T @ Su.T @ Q @ (muQ - Sx @ x0)
-    u_hat = PSI @ w_hat
-    x_hat = (Sx @ x0 + Su @ u_hat).reshape((-1, cfg.nbVar))
-    u_hat = u_hat.reshape((-1, cfg.nbVarPos))
-    return u_hat, x_hat
+    def solve(self):
+        beauty_print("Planning smooth trajectory via LQT (control primitive)", type='module')
 
+        mu, Q, R, idx_slices, tl = self.get_matrices()
+        PSI, phi = self.define_control_primitive()
+        Su, Sx = self.set_dynamical_system()
+        u_hat, x_hat = self.get_u_x(mu, Q, R, Su, Sx, PSI)
 
-def uni_cp(via_points_raw: np.ndarray, cfg: DictConfig = None):
-    print('\033[1;32m--------{}--------\033[0m'.format('Planning smooth trajectory via LQT (control primitive)'))
+        # self.vis(param, x_hat, u_hat, muQ, idx_slices, tl, phi)
+        # rf.visualab.traj_plot([x_hat[:, :2]])
+        return u_hat, x_hat, mu, idx_slices
 
-    cfg = get_config("./planning", "lqt") if cfg is None else cfg
+    def vis(self, x_hat, u_hat, muQ, idx_slices, tl, phi):
+        plt.figure()
 
-    via_points = np.zeros((len(via_points_raw), cfg.nbVar))
-    via_points[:, :cfg.nbVarPos] = via_points_raw
-    start_pose = via_points[0]
-    via_point_pose = via_points[1:]
+        plt.title("2D Trajectory")
+        plt.axis("off")
+        plt.gca().set_aspect('equal', adjustable='box')
 
-    mu, Q, R, idx_slices, tl = rf.lqt.get_matrices(cfg, via_point_pose)
-    PSI, phi = define_control_primitive(cfg)
-    Su, Sx = set_dynamical_system(cfg)
-    u_hat, x_hat = get_u_x(cfg, start_pose, mu, Q, R, Su, Sx, PSI)
+        plt.scatter(x_hat[0, 0], x_hat[0, 1], c='black', s=100)
 
-    # vis(param, x_hat, u_hat, muQ, idx_slices, tl, phi)
-    # rf.visualab.traj_plot([x_hat[:, :2]])
-    return u_hat, x_hat, mu, idx_slices
+        for slice_t in idx_slices:
+            plt.scatter(muQ[slice_t][0], muQ[slice_t][1], c='blue', s=100)
 
+        plt.plot(x_hat[:, 0], x_hat[:, 1], c='black')
 
-def vis(cfg, x_hat, u_hat, muQ, idx_slices, tl, phi):
-    plt.figure()
+        fig, axs = plt.subplots(5, 1)
 
-    plt.title("2D Trajectory")
-    plt.axis("off")
-    plt.gca().set_aspect('equal', adjustable='box')
+        axs[0].plot(x_hat[:, 0], c='black')
+        axs[0].set_ylabel("$x_1$")
+        axs[0].set_xticks([0, self.cfg.nbData])
+        axs[0].set_xticklabels(["0", "T"])
+        for t in tl:
+            axs[0].scatter(t, x_hat[t, 0], c='blue')
 
-    plt.scatter(x_hat[0, 0], x_hat[0, 1], c='black', s=100)
+        axs[1].plot(x_hat[:, 1], c='black')
+        axs[1].set_ylabel("$x_2$")
+        axs[1].set_xticks([0, self.cfg.nbData])
+        axs[1].set_xticklabels(["0", "T"])
+        for t in tl:
+            axs[1].scatter(t, x_hat[t, 1], c='blue')
 
-    for slice_t in idx_slices:
-        plt.scatter(muQ[slice_t][0], muQ[slice_t][1], c='blue', s=100)
+        axs[2].plot(u_hat[:, 0], c='black')
+        axs[2].set_ylabel("$u_1$")
+        axs[2].set_xticks([0, self.cfg.nbData - 1])
+        axs[2].set_xticklabels(["0", "T-1"])
 
-    plt.plot(x_hat[:, 0], x_hat[:, 1], c='black')
+        axs[3].plot(u_hat[:, 1], c='black')
+        axs[3].set_ylabel("$u_2$")
+        axs[3].set_xticks([0, self.cfg.nbData - 1])
+        axs[3].set_xticklabels(["0", "T-1"])
 
-    fig, axs = plt.subplots(5, 1)
+        axs[4].set_ylabel("$\phi_k$")
+        axs[4].set_xticks([0, self.cfg.nbData - 1])
+        axs[4].set_xticklabels(["0", "T-1"])
+        for i in range(self.cfg.nbFct):
+            axs[4].plot(phi[:, i])
+        axs[4].set_xlabel("$t$")
 
-    axs[0].plot(x_hat[:, 0], c='black')
-    axs[0].set_ylabel("$x_1$")
-    axs[0].set_xticks([0, cfg.nbData])
-    axs[0].set_xticklabels(["0", "T"])
-    for t in tl:
-        axs[0].scatter(t, x_hat[t, 0], c='blue')
-
-    axs[1].plot(x_hat[:, 1], c='black')
-    axs[1].set_ylabel("$x_2$")
-    axs[1].set_xticks([0, cfg.nbData])
-    axs[1].set_xticklabels(["0", "T"])
-    for t in tl:
-        axs[1].scatter(t, x_hat[t, 1], c='blue')
-
-    axs[2].plot(u_hat[:, 0], c='black')
-    axs[2].set_ylabel("$u_1$")
-    axs[2].set_xticks([0, cfg.nbData - 1])
-    axs[2].set_xticklabels(["0", "T-1"])
-
-    axs[3].plot(u_hat[:, 1], c='black')
-    axs[3].set_ylabel("$u_2$")
-    axs[3].set_xticks([0, cfg.nbData - 1])
-    axs[3].set_xticklabels(["0", "T-1"])
-
-    axs[4].set_ylabel("$\phi_k$")
-    axs[4].set_xticks([0, cfg.nbData - 1])
-    axs[4].set_xticklabels(["0", "T-1"])
-    for i in range(cfg.nbFct):
-        axs[4].plot(phi[:, i])
-    axs[4].set_xlabel("$t$")
-
-    plt.show()
+        plt.show()
