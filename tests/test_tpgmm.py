7bdc003a9ddaa45529f2256915d60be623c542ae
skylark <l.syj@163.com>
2023-03-17 11:49:19 +0800

    Add more tests

--- tests/test_tpgmm.py
+++ tests/test_tpgmm.py
@@ -3,25 +3,34 @@ import numpy as np
 import os
 
 
-# <editor-fold desc="2-dim Uni example">
-# demo_points = np.array([[[0, 0], [-1, 8], [4, 3], [2, 1], [4, 3]],
-#                         [[0, -2], [-1, 7], [3, 2.5], [2, 1.6], [4, 3]],
-#                         [[0, -1], [-1, 8], [4, 5.2], [2, 1.1], [4, 3.5]]])
-# demos_x = rf.data_generator.multi_bezier_demos(demo_points)  # (3, 50, 2): 3 demos, each has 50 points
-# model, rep = rf.tpgmm.uni(demos_x, show_demo_idx=2, plot=True)
-# </editor-fold>
+def test_2d_uni_tpgmm():
+    demo_points = np.array([[[0, 0], [-1, 8], [4, 3], [2, 1], [4, 3]],
+                            [[0, -2], [-1, 7], [3, 2.5], [2, 1.6], [4, 3]],
+                            [[0, -1], [-1, 8], [4, 5.2], [2, 1.1], [4, 3.5]]])
+    demos_x = rf.data_generator.multi_bezier_demos(demo_points)  # (3, 50, 2): 3 demos, each has 50 points
 
-# <editor-fold desc="2-dim Bi example">
-# left_demo_points = np.array([[[0, 0], [-1, 8], [4, 3], [2, 1], [4, 3]],
-#                              [[0, -2], [-1, 7], [3, 2.5], [2, 1.6], [4, 3]],
-#                              [[0, -1], [-1, 8], [4, 5.2], [2, 1.1], [4, 3.5]]])
-# right_demo_points = np.array([[[8, 8], [7, 1], [4, 3], [6, 8], [4, 3]],
-#                               [[8, 7], [7, 1], [3, 3], [6, 6], [4, 3]],
-#                               [[8, 8], [7, 1], [4, 5], [6, 8], [4, 3.5]]])
-# demos_left_x = rf.data_generator.multi_bezier_demos(left_demo_points)  # (3, 50, 2): 3 demos, each has 50 points
-# demos_right_x = rf.data_generator.multi_bezier_demos(right_demo_points)
-# model_l, model_r, rep_l, rep_r = rf.tpgmm.bi(demos_left_x, demos_right_x, show_demo_idx=2, plot=True)
-# </editor-fold>
+    representation = rf.lfd.tpgmm.TPGMM(demos_x, plot=False)
+    model = representation.fit()
+
+    # Reproductions for the same situations
+    traj = representation.reproduce(model, show_demo_idx=2)
+
+
+def test_2d_bi_tpgmm():
+    left_demo_points = np.array([[[0, 0], [-1, 8], [4, 3], [2, 1], [4, 3]],
+                                 [[0, -2], [-1, 7], [3, 2.5], [2, 1.6], [4, 3]],
+                                 [[0, -1], [-1, 8], [4, 5.2], [2, 1.1], [4, 3.5]]])
+    right_demo_points = np.array([[[8, 8], [7, 1], [4, 3], [6, 8], [4, 3]],
+                                  [[8, 7], [7, 1], [3, 3], [6, 6], [4, 3]],
+                                  [[8, 8], [7, 1], [4, 5], [6, 8], [4, 3.5]]])
+    demos_left_x = rf.data_generator.multi_bezier_demos(left_demo_points)  # (3, 50, 2): 3 demos, each has 50 points
+    demos_right_x = rf.data_generator.multi_bezier_demos(right_demo_points)
+
+    representation = rf.lfd.tpgmm.TPGMMBi(demos_left_x, demos_right_x, plot=False)
+    model_l, model_r = representation.fit()
+
+    traj_l, traj_r = representation.reproduce(model_l, model_r, show_demo_idx=2)
+
 
 def test_7d_uni_tpgmm():
     raw_demo = np.load(os.path.join(rf.utils.get_rofunc_path(), 'data/LFD_ML/LeftHand.npy'))
@@ -67,3 +76,5 @@ def test_7d_bi_tpgmm():
 if __name__ == '__main__':
     test_7d_uni_tpgmm()
     test_7d_bi_tpgmm()
+    test_2d_uni_tpgmm()
+    test_2d_bi_tpgmm()
