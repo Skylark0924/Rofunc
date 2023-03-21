ad953591f5e828f6cb59cdfa406935ca1b1dbf94
Donatien Delehelle <donatien.delehelle@iit.it>
2023-03-21 15:24:51 +0800

    Imporved processing of Optitrack files

--- rofunc/devices/optitrack/process.py
+++ rofunc/devices/optitrack/process.py
@@ -1,21 +1,58 @@
+"""Process functions for Optitrack data.
+
+Usage Example 1:
+from rofunc.devices.optitrack import get_objects, data_clean
+
+input_path = "/path/to/optitrack/data"
+objs, meta = get_objects(input_path)
+data = get_time_series(input_path, meta[0])
+
+table_pos_x = data.iloc[:, objs[0]['table']['pose']['Position']['X']]
+
+Usage Example 2:
+from rofunc.devices.optitrack import get_objects, get_time_series
+del_objects = ['cup', 'hand_right']
+objs, meta = get_objects(input_path)
+
+# Remove unused objects from the data
+for obj in del_objects:
+    del objs[obj]
+
+data, labels = data_clean(input_path, legacy=False, objs)[0]
+
+label_idx = labels.index('table.pose.x')
+table_pos_x = data[label_idx, :]
+
+"""
 import os
 import csv
 
+import pickle as pkl
 import pandas as pd
 from tqdm import tqdm
 import numpy as np
 import rofunc as rf
 
 def get_objects(input_path):
-    objs = {}
-    meta = {}
+    """Returns a dictionary of objects from the Optitrack data.
+    The Optitack csv must have the original name format (e.g. "Take 2020-06-03 15-00-00.csv").
+    The reutrned list does not necessarly have the same order as your file explorer, but the meta ond objects list do.\
+    Check the meta to make sure you work on the correct file.
+
+    Args:
+        input_path (str): path to the Optitrack data.\
+                          If the path is to a folder, it will return a list of objects for each file in the folder.
+    Returns:
+        tuple: (objects, meta)
+    """
+    objs_list = list()
+    meta_list = list()
     demo_csvs = os.listdir(input_path)
     demo_csvs = sorted(demo_csvs)
     for demo_csv in demo_csvs:
         if 'Take' in demo_csv:
-            out_path = os.path.join(input_path, 'process')
-            if not os.path.exists(out_path):
-                os.mkdir(out_path)
+            objs={}
+            meta={}
             demo_path = os.path.join(input_path, demo_csv)
             with open(demo_path) as f:
                 data = csv.reader(f)
@@ -53,41 +90,99 @@ def get_objects(input_path):
                             if tr[i] not in objs[o]['pose']:
                                 objs[o]['pose'][tr[i]] = {}
                             objs[o]['pose'][tr[i]][ax[i]] = i
+            objs_list.append(objs)
+            meta_list.append(meta)
 
-    return objs, meta
+    return objs_list, meta_list
 
 
-def data_clean(input_path):
+def data_clean(input_path, legacy=True, objs=None, save=False):
     """
+    Cleans the Optitrack data.
     Args:
-        input_path: csv file path
-
-    Returns: csv file without the first 6 lines
-
+        input_path (str): path to the Optitrack data.
+        legacy (:obj:`bool`, optional): if True, it will use the legacy version of the function.\
+                                       Defaults to True.
+        objs (:obj:`list`, optional): dictionary of objects to keep. If set to None, save all data.\
+    Returns:
+        list: list of cleaned data for all csv in folder. Type of elements in list depend on args.
     """
+    out_path = os.path.join(input_path, 'process')
+    if not os.path.exists(out_path):
+        os.mkdir(out_path)
     demo_csvs = os.listdir(input_path)
     demo_csvs = sorted(demo_csvs)
+    out_list = list()
     for demo_csv in demo_csvs:
         if 'Take' in demo_csv:
-            if 'Manus' in demo_csv:
-                out_path = os.path.join(input_path, 'process')
-                if not os.path.exists(out_path):
-                    os.mkdir(out_path)
-                demo_path = os.path.join(input_path, demo_csv)
-                out_file_path = os.path.join(out_path, demo_csv)
-                rf.utils.delete_lines(demo_path, out_file_path, 14)
-                csv_data = pd.read_csv(out_file_path)
-                # csv_data = pd.read_csv(demo_path, skiprows=6)
-                if '3f6ec26f' in demo_csv:
-                    csv_data.to_csv(os.path.join(input_path, "left_manus.csv"))
-                elif '7b28f20b' in demo_csv:
-                    csv_data.to_csv(os.path.join(input_path, "right_manus.csv"))
+            if legacy:
+                out_list.append(def_data_clean_legacy(input_path, demo_csv, out_path))
             else:
-                demo_path = os.path.join(input_path, demo_csv)
-                # The first 6 rows are headers: https://v22.wiki.optitrack.com/index.php?title=Data_Export:_CSV
-                csv_data = pd.read_csv(demo_path, skiprows=6)
-                csv_data.to_csv(os.path.join(input_path, "opti_hands.csv"))
+                if objs is None:
+                    out_data = pd.read_csv(os.path.join(input_dir, demo_csv), skiprows=6)
+                    if save:
+                        out_data.to_csv(os.path.join(out_path, demo_csv))
+                    out_list.append(out_data)
+                else:
+                    labels = ['frame', 'time']
+                    out_data = []
+                    data_raw = pd.read_csv(os.path.join(input_dir, demo_csv), skiprows=6)
+                    out_data.append(data_raw[:, 0])
+                    out_data.append(data_raw[:, 1])
+                    for obj in objs:
+                        labels.extend([f"{obj}.pose.x", f"{obj}.pose.y", f"{obj}.pose.z",
+                                       f"{obj}.pose.qx", f"{obj}.pose.qy", f"{obj}.pose.qz", f"{obj}.pose.qw"])
+                        out_data.append(data_raw[:, objs[obj]['pose']["Position"]['X']])
+                        out_data.append(data_raw[:, objs[obj]['pose']["Position"]['Y']])
+                        out_data.append(data_raw[:, objs[obj]['pose']["Position"]['Z']])
+                        out_data.append(data_raw[:, objs[obj]['pose']["Rotation"]['X']])
+                        out_data.append(data_raw[:, objs[obj]['pose']["Rotation"]['Y']])
+                        out_data.append(data_raw[:, objs[obj]['pose']["Rotation"]['Z']])
+                        out_data.append(data_raw[:, objs[obj]['pose']["Rotation"]['W']])
+                        for marker in objs[obj]['markers']:
+                            labels.extend([f"{obj}.marker.{marker}.x", f"{obj}.marker.{marker}.y", f"{obj}.marker.{marker}.z"])
+                            out_data.append(data_raw[:, objs[obj]['markers'][marker]['pose']["Position"]['X']])
+                            out_data.append(data_raw[:, objs[obj]['markers'][marker]['pose']["Position"]['Y']])
+                            out_data.append(data_raw[:, objs[obj]['markers'][marker]['pose']["Position"]['Z']])
+                    out_data = np.array(out_data).T
+                    out_list.append(out_data, labels)
+                    if save:
+                        with open(os.path.join(out_path, demo_csv.replace('.csv', '_labels.pkl')), 'wb') as f:
+                            pkl.dump(labels, f)
+                        np.save(os.path.join(out_path, demo_csv.replace('csv', 'npy')), out_data)
+
     print('{} finished'.format(input_path.split('/')[-1]))
+    return out_list
+
+
+def def_data_clean_legacy(input_path, demo_csv, out_path):
+    """
+    Cleans the Optitrack data. legacy version
+    Args:
+        input_path: path to the Optitrack data.
+        demo_csv: name of the csv file
+        out_path: path to save the cleaned data for Manus
+
+    Returns:
+        csv_data: cleaned data as a pandas dataframe
+    """
+    if 'Manus' in demo_csv:
+        demo_path = os.path.join(input_path, demo_csv)
+        out_file_path = os.path.join(out_path, demo_csv)
+        rf.utils.delete_lines(demo_path, out_file_path, 14)
+        csv_data = pd.read_csv(out_file_path)
+        # csv_data = pd.read_csv(demo_path, skiprows=6)
+        if '3f6ec26f' in demo_csv:
+            csv_data.to_csv(os.path.join(input_path, "left_manus.csv"))
+        elif '7b28f20b' in demo_csv:
+            csv_data.to_csv(os.path.join(input_path, "right_manus.csv"))
+    else:
+        demo_path = os.path.join(input_path, demo_csv)
+        # The first 6 rows are headers: https://v22.wiki.optitrack.com/index.php?title=Data_Export:_CSV
+        csv_data = pd.read_csv(demo_path, skiprows=6)
+        csv_data.to_csv(os.path.join(input_path, "opti_hands.csv"))
+
+    return csv_data
 
 
 def data_clean_batch(input_dir):
@@ -99,8 +194,6 @@ def data_clean_batch(input_dir):
 
 
 def get_time_series(input_dir, meta):
-    print('[get_time_series] Loading data...')
-    print('[get_time_series] data path: ', os.path.join(input_dir, f"{meta['Take Name']}.csv"))
     data = pd.read_csv(os.path.join(input_dir, f"{meta['Take Name']}.csv"), skiprows=6)
 
     return data
