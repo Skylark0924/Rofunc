869a31dd5f0abfb8c70d9b07e3ec36a03a72c30b
skylark <l.syj@163.com>
2023-03-25 00:05:32 +0800

    Add more coordinate algos

--- rofunc/learning/ml/tpgmm/visualize.py
+++ rofunc/learning/ml/tpgmm/visualize.py
@@ -3,65 +3,59 @@ import matplotlib.pyplot as plt
 import numpy as np
 import rofunc as rf
 
+color_list = ['steelblue', 'orangered', 'green', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan']
+
 
 def hmm_plot(demos_xdx_f, model):
-    fig, ax = plt.subplots(ncols=2, nrows=1)
-    fig.set_size_inches(12, 6)
+    P = len(demos_xdx_f[0][0])
+    fig, ax = plt.subplots(ncols=P, nrows=1)
+    fig.set_size_inches(4 * P, 6)
 
-    # position plotting
-    ax[0].set_title('pos - coord. %d' % 1)
-    for p in demos_xdx_f:
-        ax[0].plot(p[:, 0, 0], p[:, 0, 1])
-    rf.visualab.gmm_plot(model.mu, model.sigma, ax=ax[0], dim=[0, 1], color='steelblue')
-
-    ax[1].set_title('pos - coord. %d' % 2)
-    for p in demos_xdx_f:
-        ax[1].plot(p[:, 1, 0], p[:, 1, 1])
-    rf.visualab.gmm_plot(model.mu, model.sigma, ax=ax[1], dim=[4, 5], color='orangered')
+    for p in range(P):
+        ax[p].set_title('pos - coord. %d' % (p + 1))
+        for d in demos_xdx_f:
+            ax[p].plot(d[:, p, 0], d[:, p, 1])
+        rf.visualab.gmm_plot(model.mu, model.sigma, ax=ax[p], dim=[4 * p, 4 * p + 1], color=color_list[p], alpha=0.1)
 
     plt.tight_layout()
     plt.show()
 
 
 def hmm_plot_3d(demos_xdx_f, model, scale=1):
+    P = len(demos_xdx_f[0][0])
     fig = plt.figure(figsize=(4, 4))
-    fig.set_size_inches(12, 6)
+    fig.set_size_inches((4 * P, 6))
 
-    # position plotting
-    ax = fig.add_subplot(1, 2, 1, projection='3d', fc='white')
-    ax.set_title('pos - coord. %d' % 1)
-    for p in demos_xdx_f:
-        ax.plot(p[:, 0, 0], p[:, 0, 1], p[:, 0, 2])
-    rf.visualab.gmm_plot(model.mu, model.sigma, ax=ax, dim=[i for i in range(3)], color='steelblue', scale=scale,
-                         alpha=0.1)
-    rf.visualab.set_axis(ax)
-
-    ax = fig.add_subplot(1, 2, 2, projection='3d', fc='white')
-    ax.set_title('pos - coord. %d' % 2)
-    for p in demos_xdx_f:
-        ax.plot(p[:, 1, 0], p[:, 1, 1], p[:, 1, 2])
-    rf.visualab.gmm_plot(model.mu, model.sigma, ax=ax, dim=[i + 14 for i in range(3)], color='orangered', scale=scale,
-                         alpha=0.1)
-    rf.visualab.set_axis(ax)
+    for p in range(P):
+        ax = fig.add_subplot(1, P, p + 1, projection='3d', fc='white')
+        ax.set_title('pos - coord. %d' % (p + 1))
+        for d in demos_xdx_f:
+            ax.plot(d[:, p, 0], d[:, p, 1], d[:, p, 2])
+        rf.visualab.gmm_plot(model.mu, model.sigma, ax=ax, dim=[14 * p, 14 * p + 1, 14 * p + 2], color=color_list[p],
+                             scale=scale, alpha=0.1)
+        rf.visualab.set_axis(ax)
 
     plt.show()
 
 
-def poe_plot(mod1, mod2, prod, demos_x, demo_idx):
-    fig, ax = plt.subplots(ncols=3, nrows=1)
-    fig.set_size_inches((12, 6))
+def poe_plot(mod_list, prod, demos_x, demo_idx):
+    P = len(mod_list)
+    fig, ax = plt.subplots(ncols=P + 1, nrows=1)
+    fig.set_size_inches((4 * (P + 1), 6))
     for i in ax:
         i.set_aspect('equal')
 
-    ax[0].set_title('model 1')
-    rf.visualab.gmm_plot(mod1.mu, mod1.sigma, swap=True, ax=ax[0], dim=[0, 1], color='steelblue', alpha=0.3)
-    ax[1].set_title('model 2')
-    rf.visualab.gmm_plot(mod2.mu, mod2.sigma, swap=True, ax=ax[1], dim=[0, 1], color='orangered', alpha=0.3)
-    ax[2].set_title('tranformed models and product')
-    rf.visualab.gmm_plot(mod1.mu, mod1.sigma, swap=True, ax=ax[2], dim=[0, 1], color='steelblue', alpha=0.3)
-    rf.visualab.gmm_plot(mod2.mu, mod2.sigma, swap=True, ax=ax[2], dim=[0, 1], color='orangered', alpha=0.3)
-    rf.visualab.gmm_plot(prod.mu, prod.sigma, swap=True, ax=ax[2], dim=[0, 1], color='gold', alpha=0.3)
-    ax[2].plot(demos_x[demo_idx][:, 0], demos_x[demo_idx][:, 1], color="b")
+    for p in range(P):
+        ax[p].set_title('model %d' % (p + 1))
+        rf.visualab.gmm_plot(mod_list[p].mu, mod_list[p].sigma, swap=True, ax=ax[p], dim=[0, 1], color=color_list[p],
+                             alpha=0.3)
+
+    ax[P].set_title('tranformed models and product')
+    for p in range(P):
+        rf.visualab.gmm_plot(mod_list[p].mu, mod_list[p].sigma, swap=True, ax=ax[P], dim=[0, 1], color=color_list[p],
+                             alpha=0.3)
+    rf.visualab.gmm_plot(prod.mu, prod.sigma, swap=True, ax=ax[P], dim=[0, 1], color='gold', alpha=0.3)
+    ax[P].plot(demos_x[demo_idx][:, 0], demos_x[demo_idx][:, 1], color="b")
 
     patches = [mpatches.Patch(color='steelblue', label='transformed model 1'),
                mpatches.Patch(color='orangered', label='transformed model 2'),
@@ -71,24 +65,23 @@ def poe_plot(mod1, mod2, prod, demos_x, demo_idx):
     plt.show()
 
 
-def poe_plot_3d(mod1, mod2, prod, demos_x, demo_idx):
+def poe_plot_3d(mod_list, prod, demos_x, demo_idx):
+    P = len(mod_list)
     fig = plt.figure(figsize=(4, 4))
-    fig.set_size_inches((12, 6))
+    fig.set_size_inches((4 * (P + 1), 6))
 
-    ax = fig.add_subplot(1, 3, 1, projection='3d', fc='white')
-    ax.set_title('model 1')
-    rf.visualab.gmm_plot(mod1.mu, mod1.sigma, swap=True, ax=ax, dim=[0, 1, 2], color='steelblue', alpha=0.05)
-    rf.visualab.set_axis(ax)
+    for p in range(P):
+        ax = fig.add_subplot(1, P + 1, p + 1, projection='3d', fc='white')
+        ax.set_title('model %d' % (p + 1))
+        rf.visualab.gmm_plot(mod_list[p].mu, mod_list[p].sigma, swap=True, ax=ax, dim=[0, 1, 2], color=color_list[p],
+                             alpha=0.05)
+        rf.visualab.set_axis(ax)
 
-    ax = fig.add_subplot(1, 3, 2, projection='3d', fc='white')
-    ax.set_title('model 2')
-    rf.visualab.gmm_plot(mod2.mu, mod2.sigma, swap=True, ax=ax, dim=[0, 1, 2], color='orangered', alpha=0.05)
-    rf.visualab.set_axis(ax)
-
-    ax = fig.add_subplot(1, 3, 3, projection='3d', fc='white')
+    ax = fig.add_subplot(1, 4, 4, projection='3d', fc='white')
     ax.set_title('transformed models and product')
-    rf.visualab.gmm_plot(mod1.mu, mod1.sigma, swap=True, ax=ax, dim=[0, 1, 2], color='steelblue', alpha=0.05)
-    rf.visualab.gmm_plot(mod2.mu, mod2.sigma, swap=True, ax=ax, dim=[0, 1, 2], color='orangered', alpha=0.05)
+    for p in range(P):
+        rf.visualab.gmm_plot(mod_list[p].mu, mod_list[p].sigma, swap=True, ax=ax, dim=[0, 1, 2], color=color_list[p],
+                             alpha=0.05)
     rf.visualab.gmm_plot(prod.mu, prod.sigma, swap=True, ax=ax, dim=[0, 1, 2], color='gold', alpha=0.05)
     ax.plot(demos_x[demo_idx][:, 0], demos_x[demo_idx][:, 1], demos_x[demo_idx][:, 2], color="b")
     rf.visualab.set_axis(ax)
