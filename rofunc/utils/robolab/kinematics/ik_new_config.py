dd4a633645ee832627c7f3775210eab4d4f3c26e
Lee950507 <lichenzui1995@gmail.com>
2023-04-21 00:52:28 +0800

    ik of new config (without constraint)

--- rofunc/utils/robolab/kinematics/ik_new_config.py
+++ rofunc/utils/robolab/kinematics/ik_new_config.py
@@ -9,16 +9,17 @@ from numpy.linalg import norm, solve
 
 
 def transform_torso(torso_joint):
-    T_1 = np.array([[math.cos(torso_joint[0]), -math.sin(torso_joint[0]), 0, 0.2375],
+    T_torso = np.empty([len(torso_joint), 4, 4])
+    T_torso[0] = np.array([[math.cos(torso_joint[0]), -math.sin(torso_joint[0]), 0, 0.2375],
                           [math.sin(torso_joint[0]), math.cos(torso_joint[0]), 0, 0], [0, 0, 1, 0.75262], [0, 0, 0, 1]])
-    T_2 = np.array(
+    T_torso[1] = np.array(
         [[1, 0, 0, 0.25 * math.sin(torso_joint[1])], [0, 1, 0, 0],
          [0, 0, 1, 0.25 * math.cos(torso_joint[1])], [0, 0, 0, 1]])
-    T_3 = np.array(
+    T_torso[2] = np.array(
         [[1, 0, 0, 0.25 * math.cos(torso_joint[1] + torso_joint[2])], [0, 1, 0, 0],
          [0, 0, 1, 0.25 * -math.sin(torso_joint[1] + torso_joint[2])], [0, 0, 0, 1]])
 
-    return T_1, T_2, T_3
+    return T_torso
 
 
 def transform_left_arm(arm_joint):
@@ -32,9 +33,9 @@ def transform_left_arm(arm_joint):
     a_franka = DH[:, 1]
     d_franka = DH[:, 2]
     q_franka = DH[:, 3]
-    T = np.empty([len(DH), 4, 4])
+    T_arm = np.empty([len(DH), 4, 4])
     for i in range(len(DH)):
-        T[i, :, :] = np.array([[math.cos(q_franka[i]), -math.sin(q_franka[i]), 0, a_franka[i]],
+        T_arm[i, :, :] = np.array([[math.cos(q_franka[i]), -math.sin(q_franka[i]), 0, a_franka[i]],
                                [math.sin(q_franka[i]) * math.cos(alpha_franka[i]),
                                 math.cos(q_franka[i]) * math.cos(alpha_franka[i]), -math.sin(alpha_franka[i]),
                                 -math.sin(alpha_franka[i]) * d_franka[i]],
@@ -42,57 +43,66 @@ def transform_left_arm(arm_joint):
                                 math.cos(q_franka[i]) * math.sin(alpha_franka[i]), math.cos(alpha_franka[i]),
                                 math.cos(alpha_franka[i]) * d_franka[i]],
                                [0, 0, 0, 1]])
-    T_4 = T_TorsoEndToLeftArmBase @ T[0]
-    T_5 = T[1]
-    T_6 = T[2]
-    T_7 = T[3]
-    T_8 = T[4]
-    T_9 = T[5]
-    T_10 = T[6]
+    T_arm[0] = T_TorsoEndToLeftArmBase @ T_arm[0]
 
-    return T_4, T_5, T_6, T_7, T_8, T_9, T_10
+    return T_arm
 
 
-T_1, T_2, T_3 = transform_torso(np.array([0, -0.7, 0.1]))
-T_4, T_5, T_6, T_7, T_8, T_9, T_10 = transform_left_arm(np.array([0, 0, 0, 0, 0, 0, 0]))
+def compute_jacobian(T):
+    p = np.zeros([len(T) + 1, 3])
+    z = np.empty([len(T), 3])
+    T_new = np.zeros([len(T), 4, 4])
+    J = np.empty([len(T), 6])
+    for i in range(len(T)):
+        T_new[i] = np.eye(4)
+        for j in range(i + 1):
+            T_new[i] = T_new[i] @ T[j]
+        p[i + 1] = (T_new[i, 0:3, 0:3] @ p[0].T + T_new[i, 0:3, 3]).T
+        z[i] = T_new[i, 0:3, 2]
+    for i in range(len(T)):
+        J[i] = np.append(np.cross(z[i], (p[len(T)] - p[i])), z[i])
+    return J
 
-T_test = T_1 @ T_2 @ T_3 @ T_4 @ T_5 @ T_6 @ T_7 @ T_8 @ T_9 @ T_10
-print(T_test)
-# model = pinocchio.buildSampleModelManipulator()
-# data = model.createData()
 
-JOINT_ID = 10
-oMdes = pinocchio.SE3(np.eye(3), np.array([1., 0., 1.]))
+def ik_new_config(pose, ori, joint_id):
+    q = np.array([0, -0.7, 0.1, 0, 0, 0, 0, 0, 0, 0])   #initial joint states
+    oMdes = pinocchio.SE3(ori, pose)
+    eps = 1e-4
+    IT_MAX = 1000
+    DT = 1e-1
+    damp = 1e-12
 
-q = np.zeros(10)
-eps = 1e-4
-IT_MAX = 1000
-DT = 1e-1
-damp = 1e-12
+    i = 0
+    while True:
+        oMi_l, oMi_r = transform_mobilebasetoarmend(q[0:3], q[3:10], q[3:10])
+        T_overall = np.append(transform_torso(q[0:3]), transform_left_arm(q[3:10]), axis=0)
+        oMi = pinocchio.SE3(oMi_l[0:3, 0:3], oMi_l[0:3, 3])
+        dMi = oMdes.actInv(oMi)
+        err = pinocchio.log(dMi).vector
+        if norm(err) < eps:
+            success = True
+            break
+        if i >= IT_MAX:
+            success = False
+            break
+        J = compute_jacobian(T_overall[:joint_id]).T
+        v = - J.T.dot(solve(J.dot(J.T) + damp * np.eye(6), err))
+        q = q + v * DT
+        if not i % 10:
+            print('%d: error = %s' % (i, err.T))
+        i += 1
 
-i = 0
-while True:
-    oMi_l, oMi_r = transform_mobilebasetoarmend(q[0:3], q[3:10], q[3:10])
-    oMi = pinocchio.SE3(oMi_l[0:3, 0:3], oMi_l[0:3, 3])
-    dMi = oMdes.actInv(oMi)
-    err = pinocchio.log(dMi).vector
-    if norm(err) < eps:
-        success = True
-        break
-    if i >= IT_MAX:
-        success = False
-        break
-    J = pinocchio.computeJointJacobian(model, data, q, JOINT_ID)
-    v = - J.T.dot(solve(J.dot(J.T) + damp * np.eye(6), err))
-    q = pinocchio.integrate(model, q, v * DT)
-    if not i % 10:
-        print('%d: error = %s' % (i, err.T))
-    i += 1
+    if success:
+        print("Convergence achieved!")
+    else:
+        print("\nWarning: the iterative algorithm has not reached convergence to the desired precision")
 
-if success:
-    print("Convergence achieved!")
-else:
-    print("\nWarning: the iterative algorithm has not reached convergence to the desired precision")
+    print('\nresult: %s' % q.flatten().tolist())
+    print('\nfinal error: %s' % err.T)
+    return q
 
-print('\nresult: %s' % q.flatten().tolist())
-print('\nfinal error: %s' % err.T)
+
+if __name__ == '__main__':
+    pose = np.array([1, 0, 1])
+    ori = np.eye(3)
+    q = ik_new_config(pose, ori, joint_id=10)
