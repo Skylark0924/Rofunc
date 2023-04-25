c4b5b50345cf43865dc65d8d1cf163f8615e92e4
Lee950507 <lichenzui1995@gmail.com>
2023-04-26 01:31:15 +0800

    update

--- rofunc/utils/robolab/kinematics/fk_new_config.py
+++ rofunc/utils/robolab/kinematics/fk_new_config.py
@@ -56,7 +56,10 @@ def transform_mobilebasetoarmbase(torso_joint):
 
 def transform_armbasetoarmend(arm_joint):
     # create DH parameters given by franka-emika
-    DH = np.array([[0, 0, 0.333, arm_joint[0]], [-math.pi/2, 0, 0, arm_joint[1]], [math.pi/2, 0, 0.316, arm_joint[2]], [math.pi/2, 0.0825, 0, arm_joint[3]], [-math.pi/2, -0.0825, 0.384, arm_joint[4]], [math.pi/2, 0, 0, arm_joint[5]], [math.pi/2, 0.088, 0.107, arm_joint[6]]])
+    DH = np.array(
+        [[0, 0, 0.333, arm_joint[0]], [-math.pi / 2, 0, 0, arm_joint[1]], [math.pi / 2, 0, 0.316, arm_joint[2]],
+         [math.pi / 2, 0.0825, 0, arm_joint[3]], [-math.pi / 2, -0.0825, 0.384, arm_joint[4]],
+         [math.pi / 2, 0, 0, arm_joint[5]], [math.pi / 2, 0.088, 0.107, arm_joint[6]]])
     alpha_franka = DH[:, 0]
     a_franka = DH[:, 1]
     d_franka = DH[:, 2]
@@ -64,11 +67,15 @@ def transform_armbasetoarmend(arm_joint):
     T = np.empty([len(DH), 4, 4])
     for i in range(len(DH)):
         T[i, :, :] = np.array([[math.cos(q_franka[i]), -math.sin(q_franka[i]), 0, a_franka[i]],
-                    [math.sin(q_franka[i]) * math.cos(alpha_franka[i]), math.cos(q_franka[i]) * math.cos(alpha_franka[i]), -math.sin(alpha_franka[i]), -math.sin(alpha_franka[i]) * d_franka[i]],
-                    [math.sin(q_franka[i]) * math.sin(alpha_franka[i]), math.cos(q_franka[i]) * math.sin(alpha_franka[i]),  math.cos(alpha_franka[i]), math.cos(alpha_franka[i]) * d_franka[i]],
-                    [0,  0,  0,  1]])
+                               [math.sin(q_franka[i]) * math.cos(alpha_franka[i]),
+                                math.cos(q_franka[i]) * math.cos(alpha_franka[i]), -math.sin(alpha_franka[i]),
+                                -math.sin(alpha_franka[i]) * d_franka[i]],
+                               [math.sin(q_franka[i]) * math.sin(alpha_franka[i]),
+                                math.cos(q_franka[i]) * math.sin(alpha_franka[i]), math.cos(alpha_franka[i]),
+                                math.cos(alpha_franka[i]) * d_franka[i]],
+                               [0, 0, 0, 1]])
     T_ArmBaseToArmEnd = np.around(T[0] @ T[1] @ T[2] @ T[3] @ T[4] @ T[5] @ T[6], decimals=6)
-    return T_ArmBaseToArmEnd      # Arm end represents the flange of Franka Emila
+    return T_ArmBaseToArmEnd  # Arm end represents the flange of Franka Emila
 
 
 def transform_mobilebasetoarmend(torso_joint, arm_joint_left, arm_joint_right):
@@ -81,25 +88,20 @@ def transform_mobilebasetoarmend(torso_joint, arm_joint_left, arm_joint_right):
 def transform_worldbasetomobilebase(data):
     translation = np.array([[data[0], data[1], 0]])
     rotation = np.array([[math.cos(data[2]), -math.sin(data[2]), 0], [math.sin(data[2]), math.cos(data[2]), 0],
-         [0, 0, 1]])
+                         [0, 0, 1]])
     T_WorldBaseToMobileBase = np.r_[np.c_[rotation, translation.T], np.array([[0, 0, 0, 1]])]
     return T_WorldBaseToMobileBase
 
 
-def transform_optitrackbasetomobilebase(data):
-    translation = np.array([[0, -1.11462, -0.2195]])
-    R_1 =np.array([[math.cos(data[0]), 0, math.sin(data[0])], [0, 1, 0],
-                    [-math.sin(data[0]), 0, math.cos(data[0])]])
-    R_2 = np.array([[1, 0, 0], [0, math.cos(data[1]), -math.sin(data[1])],
-                    [0, math.sin(data[1]), math.cos(data[1])]])
-    rotation = R_1 @ R_2
-    # rotation = np.array([[0, -1, 0], [0, 0, 1], [-1, 0, 0]]).T
+def transform_optitrackbasetomobilebase():
+    translation = np.array([[0, 0.2195, -1.11462]])
+    rotation = np.array([[0, 1, 0], [1, 0, 0], [0, 0, 1]])
     T_OptitrackBaseToMobileBase = np.r_[np.c_[rotation, translation.T], np.array([[0, 0, 0, 1]])]
     return T_OptitrackBaseToMobileBase
 
 
 def transform_optitrackbasetoarmend(torso_joint, arm_joint_left, arm_joint_right):
-    T_OptitrackBaseToMobileBase = transform_optitrackbasetomobilebase(np.array([math.pi / 2, -math.pi / 2]))
+    T_OptitrackBaseToMobileBase = transform_optitrackbasetomobilebase()
     T_MobileBaseToLeftArmEnd, T_MobileBaseToRightArmEnd = transform_mobilebasetoarmend(torso_joint, arm_joint_left,
                                                                                        arm_joint_right)
     T_OptitrackBaseToLeftArmEnd = T_OptitrackBaseToMobileBase @ T_MobileBaseToLeftArmEnd
@@ -107,34 +109,11 @@ def transform_optitrackbasetoarmend(torso_joint, arm_joint_left, arm_joint_right
     return T_OptitrackBaseToLeftArmEnd, T_OptitrackBaseToRightArmEnd
 
 
-
 if __name__ == '__main__':
-    # T_LeftArmBaseToArmEnd = transform_armbasetoarmend(np.array([0, 0, 0, 0, 0, 0, 0]))
-    # T_RightArmBaseToArmEnd = transform_armbasetoarmend(np.array([0, 0, 0, 0, 0, 0, 0]))
-    # T_MobileBaseToLeftArmBase, T_MobileBaseToRightArmBase = transform_mobilebasetoarmbase(np.array([0, -0.7, 0.1]))
-    # print(T_MobileBaseToLeftArmBase, '\n', T_MobileBaseToRightArmBase)
-
-    T_OptitrackBaseToMobileBase = transform_optitrackbasetomobilebase(np.array([math.pi/2, -math.pi/2]))
-    pose = np.array([[1, 0, 0, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]])
-    pose_new = np.linalg.inv(T_OptitrackBaseToMobileBase) @ pose
-    print(pose_new)
-
-    # T_TorsoEndToLeftArmBase = transform_torsoendtoarmbase(
-    #     np.array([math.pi / 2, -math.pi / 4, -math.pi / 6, -math.pi / 18]), np.array([[-0.08537, 0.07009, 0.2535]]))
-    # T_TorsoEndToRightArmBase = transform_torsoendtoarmbase(
-    #     np.array([math.pi / 2, math.pi / 4, math.pi / 6, -math.pi / 18]), np.array([[-0.08537, -0.07009, 0.2535]]))
-    # print(T_TorsoEndToLeftArmBase, '\n', T_TorsoEndToRightArmBase)
-
     torso_joint = np.array([0, -0.7, 0.1])
     arm_joint_left = np.array([0, 0, 0, 0, 0, 0, 0])
     arm_joint_right = np.array([0, 0, 0, 0, 0, 0, 0])
     # T_MobileBaseToLeftArmEnd, T_MobileBaseToRightArmEnd = transform_mobilebasetoarmend(torso_joint, arm_joint_left, arm_joint_right)
     # print(T_MobileBaseToLeftArmEnd, '\n', T_MobileBaseToRightArmEnd)
-    # test = T_OptitrackBaseToMobileBase @ T_MobileBaseToLeftArmEnd
     T_l, T_r = transform_optitrackbasetoarmend(torso_joint, arm_joint_left, arm_joint_right)
     print(T_l, '\n', T_r)
-
-
-    # T_WorldBaseToMobileBase = transform_worldbasetomobilebase(np.array([1, 1, 0]))    # x, y, theta
-    # print(T_WorldBaseToMobileBase)
-
