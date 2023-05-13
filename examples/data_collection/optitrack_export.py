9d4406dd8ae49737fed1ac9803333e54ef4ce5a4
skylark <l.syj@163.com>
2023-05-13 21:05:31 +0800

    Update optitrack code

--- examples/data_collection/optitrack_export.py
+++ examples/data_collection/optitrack_export.py
@@ -9,8 +9,9 @@ import rofunc as rf
 import os
 import numpy as np
 
-data = rf.optitrack.export('/home/ubuntu/Data/optitrack/Take 2022-12-21 05.41.07 PM.csv')
+input_path = os.path.join(rf.utils.get_rofunc_path(), 'data/RAW_DEMO/optitrack/Take 2023-03-29 06.23.40 PM.csv')
+data = rf.optitrack.export(input_path)
 
 root_dir = '/home/ubuntu/Data/optitrack'
-exp_name = 'rigid_body'
-np.save(os.path.join(root_dir, exp_name), data)
+# exp_name = 'rigid_body'
+# np.save(os.path.join(root_dir, exp_name), data)
