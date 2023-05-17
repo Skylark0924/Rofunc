a5415a8551bdc7333ef73caa8dceb00a779da327
skylark <l.syj@163.com>
2023-05-17 17:50:10 +0800

    Update xsens visualize

--- examples/data_collection/xsens_export.py
+++ examples/data_collection/xsens_export.py
@@ -8,9 +8,9 @@ This example shows how to export Xsens MVNX file to multiple skeleton .npy files
 import rofunc as rf
 
 # Export a single mvnx file
-mvnx_file = '../xsens_data/test.mvnx'
-rf.xsens.export(mvnx_file, output_type='joint')
+mvnx_file = '/home/ubuntu/Data/xsens_mvnx/2023_05_16_joint_angle_test/010-003.mvnx'
+rf.xsens.export(mvnx_file)
 
 # Export a batch of mvnx files in a directory
-mvnx_dir = '../xsens_data'
-rf.xsens.export_batch(mvnx_dir)
+# mvnx_dir = '../xsens_data'
+# rf.xsens.export_batch(mvnx_dir)
