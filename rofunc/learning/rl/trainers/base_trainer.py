8c4da338a033c01a092d95248b9bc2afef39de63
Junjia Liu <l.syj@163.com>
2023-06-09 21:24:38 +0800

    [Rofunc RL] RofuncPPO 1.5x better than SKRL in two Isaac robot tasks

--- rofunc/learning/rl/trainers/base_trainer.py
+++ rofunc/learning/rl/trainers/base_trainer.py
@@ -36,8 +36,8 @@ class BaseTrainer:
         if not directory:
             directory = os.path.join(os.getcwd(), "runs")
         if not experiment_name:
-            experiment_name = "RofuncRL_{}_{}".format(self.__class__.__name__,
-                                                      datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S-%f"))
+            experiment_name = "RofuncRL_{}_{}_{}".format(self.__class__.__name__, self.cfg.Trainer.task_name,
+                                                         datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S-%f"))
         self.experiment_dir = os.path.join(directory, experiment_name)
         rf.utils.create_dir(self.experiment_dir)
 
