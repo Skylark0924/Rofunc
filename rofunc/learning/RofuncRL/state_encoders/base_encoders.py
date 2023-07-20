a81b34ac604beca050ada81b9016397703ee9bcd
Junjia Liu <l.syj@163.com>
2023-07-20 18:21:32 +0800

    🚀 Add state Encoders
    BLPEncoder, CNNEncoder, ResnetEncoder

--- rofunc/learning/RofuncRL/state_encoders/base_encoders.py
+++ rofunc/learning/RofuncRL/state_encoders/base_encoders.py
@@ -14,47 +14,24 @@
  limitations under the License.
  """
 
-import torch
 import torch.nn as nn
+from omegaconf import DictConfig
 
-
-class NnReshape(nn.Module):
-    def __init__(self, *args):
-        super().__init__()
-        self.args = args
-
-    def forward(self, x):
-        return x.view((x.size(0),) + self.args)
+from rofunc.config.utils import omegaconf_to_dict
+from rofunc.learning.RofuncRL.models.base_models import BaseMLP
+from rofunc.learning.RofuncRL.models.utils import activation_func
 
 
 class BaseEncoder(nn.Module):
-    def __init__(self, recurrent, recurrent_input_size, hidden_size):
+    def __init__(self, cfg: DictConfig, input_dim: int, output_dim: int, cfg_name: str = 'encoder'):
         super(BaseEncoder, self).__init__()
 
-        self._hidden_size = hidden_size
-        self._recurrent = recurrent
+        self.cfg = cfg
+        self.cfg_dict = omegaconf_to_dict(self.cfg)
+        self.hidden_dims = self.cfg_dict[cfg_name]['hidden_dims']
+        self.activation = activation_func(self.cfg_dict[cfg_name]['activation'])
 
 
-class CNNEncoder(BaseEncoder):
-    def __init__(self, num_inputs, recurrent=False, hidden_size=512):
-        super(CNNEncoder, self).__init__(recurrent, hidden_size, hidden_size)
-
-        init_ = lambda m: init(m, nn.init.orthogonal_, lambda x: nn.init.
-                               constant_(x, 0), nn.init.calculate_gain('relu'))
-
-        # input: [1, 16, 64, 64]
-        self.main = nn.Sequential(
-            init_(nn.Conv2d(num_inputs, 8, 8, stride=4)), nn.ReLU(),  # [8, 15, 15]
-            init_(nn.Conv2d(8, 16, 4, stride=2)), nn.ReLU(),  # [16, 6, 6]
-            # init_(nn.Conv2d(16, 8, 3, stride=1)), nn.ReLU(),  # [8, 4, 4]
-            Flatten(),  # [4608]
-            init_(nn.Linear(16 * 6 * 6, hidden_size)), nn.ReLU())
-
-        self.train()
-
-    def forward(self, inputs):
-        x = self.main(inputs)
-        return x
-
-
-
+class MLPEncoder(BaseMLP):
+    def __init__(self, cfg, input_dim, output_dim, cfg_name):
+        super().__init__(cfg, input_dim, output_dim, cfg_name)
--- rofunc/learning/RofuncRL/state_encoders/visual_encoders.py
+++ rofunc/learning/RofuncRL/state_encoders/visual_encoders.py
@@ -13,10 +13,58 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  """
+import torch
+import torch.nn as nn
 
-from .base_encoders import BaseEncoder
+from rofunc.learning.RofuncRL.models.utils import build_mlp, build_cnn, init_layers
+from rofunc.learning.RofuncRL.state_encoders.base_encoders import BaseEncoder
+
+
+class CNNEncoder(BaseEncoder):
+    def __init__(self, cfg, input_dim, output_dim):
+        super().__init__(cfg, input_dim, output_dim)
+
+        self.cnn_kernel_size = self.cfg_dict['encoder']['cnn_kernel_size']
+        self.cnn_stride = self.cfg_dict['encoder']['cnn_stride']
+        self.cnn_padding = self.cfg_dict['encoder']['cnn_padding']
+        self.cnn_dilation = self.cfg_dict['encoder']['cnn_dilation']
+
+        self.backbone_net = build_cnn(dims=[input_dim, *self.hidden_dims, output_dim], kernel_size=self.cnn_kernel_size,
+                                      stride=self.cnn_stride, padding=self.cnn_padding, dilation=self.cnn_dilation,
+                                      hidden_activation=self.activation)
+        self.flatten = nn.Flatten(start_dim=1)
+        self.output_net = nn.Linear(3 * 3 * self.hidden_dims[-1], output_dim)
+
+        if self.cfg.use_init:
+            init_layers(self.backbone_net, gain=1.0, init_type='kaiming_uniform')
+
+    def forward(self, inputs):
+        x = self.backbone_net(inputs)
+        x = self.flatten(x)
+        x = self.output_net(x)
+        return x
+
+
+class ResnetEncoder(BaseEncoder):
+    def __init__(self, cfg, input_dim, output_dim):
+        super().__init__(cfg, input_dim, output_dim)
+
+        self.resnet_type = self.cfg_dict['encoder']['resnet_type']
+        assert self.resnet_type in ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152']
+        self.resnet_pre_trained = self.cfg_dict['encoder']['resnet_pre_trained']
+
+        self.backbone_net = torch.hub.load('pytorch/vision', 'resnet18', weights="IMAGENET1K_V2",
+                                           pretrained=self.resnet_pre_trained)
+
+    def forward(self, inputs):
+        x = self.net(inputs)
+        return x
 
 
 class ViTEncoder(BaseEncoder):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, cfg, input_dim, output_dim):
+        super().__init__(cfg, input_dim, output_dim)
+
+        self.vit_type = self.cfg_dict['encoder']['vit_type']
+        assert self.vit_type in ['']
+        self.vit_pre_trained = self.cfg_dict['encoder']['vit_pre_trained']
