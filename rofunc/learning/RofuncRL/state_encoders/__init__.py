7c65447a6ac1730173daabb61a51d662e7a7fabd
Junjia Liu <l.syj@163.com>
2023-07-21 00:16:00 +0800

    🚀 [RofuncRL] Add state encoders to RL algos

--- rofunc/learning/RofuncRL/state_encoders/__init__.py
+++ rofunc/learning/RofuncRL/state_encoders/__init__.py
@@ -1,8 +1,9 @@
-from .base_encoders import BaseEncoder, MLPEncoder
+from .base_encoders import EmptyEncoder, BaseEncoder, MLPEncoder
 from .visual_encoders import CNNEncoder, ResnetEncoder, ViTEncoder
 from .graph_encoders import HomoGraphEncoder, HeteroGraphEncoder
 
 encoder_map = {
+    'empty': EmptyEncoder,
     # Encoders for vector data
     'mlp': MLPEncoder,
     # Encoders for image data
