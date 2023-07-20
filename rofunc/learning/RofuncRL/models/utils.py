71232eface7f39832a67f993a5924968e725c460
Junjia Liu <l.syj@163.com>
2023-07-20 18:21:32 +0800

    🚀 Add state Encoders
    BLPEncoder, CNNEncoder, ResnetEncoder

--- rofunc/learning/RofuncRL/models/utils.py
+++ rofunc/learning/RofuncRL/models/utils.py
@@ -14,9 +14,10 @@
  limitations under the License.
  """
 
+from typing import Union, List
+
 import gym
 import gymnasium
-import torch
 import torch.nn as nn
 
 
@@ -37,6 +38,44 @@ def build_mlp(dims: [int], hidden_activation: nn = nn.ReLU, output_activation: n
     return nn.Sequential(*layers)
 
 
+def build_cnn(dims: [int], kernel_size: Union[int, tuple, List], stride: Union[int, tuple, List] = 1,
+              padding: Union[int, tuple, List] = 0, dilation: Union[int, tuple, List] = 1,
+              hidden_activation: nn = nn.ReLU, output_activation: nn = None) -> nn.Sequential:
+    """
+    Build convolutional neural network
+    :param dims: layer dimensions, including input and output layers
+    :param kernel_size: kernel size for convolutional layers
+    :param stride: stride controls the stride for the cross-correlation
+    :param padding: padding controls the amount of padding applied to the input
+    :param dilation: dilation controls the spacing between the kernel points
+    :param hidden_activation: activation function for hidden layers
+    :param output_activation: activation function for output layer
+
+    The parameters kernel_size, stride, padding, dilation can either be:
+    - a single int: in which case the same value is used for the height and width dimension
+    - a tuple of two ints: in which case, the first int is used for the height dimension, and the second int for the width dimension
+    :return:
+    """
+    if isinstance(kernel_size, int) or isinstance(kernel_size, tuple):
+        kernel_size = [kernel_size] * (len(dims) - 1)
+    if isinstance(stride, int) or isinstance(stride, tuple):
+        stride = [stride] * (len(dims) - 1)
+    if isinstance(padding, int) or isinstance(padding, tuple):
+        padding = [padding] * (len(dims) - 1)
+    if isinstance(dilation, int) or isinstance(dilation, tuple):
+        dilation = [dilation] * (len(dims) - 1)
+
+    layers = []
+    for i in range(len(dims) - 1):
+        layers.append(
+            nn.Conv2d(dims[i], dims[i + 1], kernel_size=kernel_size[i], stride=stride[i], padding=padding[i],
+                      dilation=dilation[i]))
+        layers.append(hidden_activation)
+    if output_activation is not None:
+        layers.append(output_activation)
+    return nn.Sequential(*layers)
+
+
 def activation_func(activation: str) -> nn:
     if activation == 'relu':
         return nn.ReLU()
@@ -50,24 +89,33 @@ def activation_func(activation: str) -> nn:
         raise NotImplementedError
 
 
-def init_layers(layers, gain=1.0, bias_const=1e-6):
+def init_layers(layers, gain=1.0, bias_const=1e-6, init_type='orthogonal'):
+    # For more information about the initialization, please refer to https://pytorch.org/docs/stable/nn.init.html
+    init_type_map = {
+        "orthogonal": nn.init.orthogonal_,
+        "xavier_normal": nn.init.xavier_normal_,
+        "xavier_uniform": nn.init.xavier_uniform_,
+        "kaiming_normal": nn.init.kaiming_normal_,
+        "kaiming_uniform": nn.init.kaiming_uniform_,
+        "normal": nn.init.normal_,
+    }
+    torch_init = init_type_map[init_type]
+
     if not isinstance(layers, list):
         layers = [layers]
     for layer in layers:
         if isinstance(layer, nn.Linear):
-            init_with_orthogonal(layer, gain, bias_const)
+            torch_init(layer.weight, gain=gain)
+        elif isinstance(layer, nn.Conv2d):
+            assert init_type in ['kaiming_normal', 'kaiming_uniform', 'xavier_uniform']
+            torch_init(layer.weight, gain=gain)
+        elif isinstance(layer, nn.LayerNorm):
+            nn.init.constant_(layer.weight, 1)
+        elif isinstance(layer, nn.BatchNorm2d):
+            nn.init.constant_(layer.weight, 1)
 
-
-def init_with_orthogonal(layer, gain=1.0, bias_const=1e-6):
-    """
-    Parameter orthogonal initialization
-    :param layer:
-    :param gain:
-    :param bias_const:
-    :return:
-    """
-    torch.nn.init.orthogonal_(layer.weight, gain=gain)
-    torch.nn.init.constant_(layer.bias, bias_const)
+        if layer.bias is not None:
+            nn.init.constant_(layer.bias, bias_const)
 
 
 def get_space_dim(space):
