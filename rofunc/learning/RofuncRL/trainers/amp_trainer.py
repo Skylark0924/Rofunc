f84963fa1ab40a045c1125e41d7a9c6da43fdda3
Junjia Liu <l.syj@163.com>
2023-08-18 22:14:59 +0800

    Update copyright

--- rofunc/learning/RofuncRL/trainers/amp_trainer.py
+++ rofunc/learning/RofuncRL/trainers/amp_trainer.py
@@ -12,7 +12,7 @@
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
- """
+"""
 
 from rofunc.learning.RofuncRL.agents.mixline.amp_agent import AMPAgent
 from rofunc.learning.RofuncRL.trainers.base_trainer import BaseTrainer
