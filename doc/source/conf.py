c3f370561958b443fe73a99bfc12023db18887e5
Junjia Liu <l.syj@163.com>
2023-08-19 15:03:20 +0800

    📚  Update copyright

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -170,7 +170,7 @@ html_theme_options = {
     "use_edit_page_button": True,
     # "use_source_button": True,
     "use_issues_button": True,
-    # "use_repository_button": True,
+    "use_repository_button": True,
     "use_download_button": True,
     "use_sidenotes": True,
     "icon_links": [
--- rofunc/learning/RofuncRL/utils/skrl_utils.py
+++ rofunc/learning/RofuncRL/utils/skrl_utils.py
@@ -1,26 +1,22 @@
-"""
- Copyright 2023, Junjia LIU, jjliu@mae.cuhk.edu.hk
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-      https://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-"""
+# Copyright 2023, Junjia LIU, jjliu@mae.cuhk.edu.hk
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import datetime
 import os
-import sys
 
 import torch
 import torch.nn as nn
-from hydra._internal.utils import get_args_parser
 from skrl.agents.torch.a2c import A2C as A2CAgent
 from skrl.agents.torch.amp import AMP as AMPAgent
 from skrl.agents.torch.ppo import PPO as PPOAgent
