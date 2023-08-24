70db278bad5f5d908725be33b4e43d152fb175c7
Skylark <l.syj@163.com>
2023-08-24 18:24:22 +0800

    🚀 [RofuncRL] Add motion visualization

--- rofunc/learning/RofuncRL/tasks/ase/humanoid_view_motion.py
+++ rofunc/learning/RofuncRL/tasks/ase/humanoid_view_motion.py
@@ -33,11 +33,10 @@ from isaacgym import gymtorch
 from .humanoid_amp import HumanoidAMP
 
 
-class HumanoidViewMotion(HumanoidAMP):
+class HumanoidViewMotionTask(HumanoidAMP):
     def __init__(self, cfg, rl_device, sim_device, graphics_device_id, headless, virtual_screen_capture, force_render):
         self.cfg = cfg
         control_freq_inv = cfg["env"]["controlFrequencyInv"]
-        self._motion_dt = control_freq_inv * sim_params.dt
 
         cfg["env"]["controlFrequencyInv"] = 1
         cfg["env"]["pdControl"] = False
@@ -45,15 +44,18 @@ class HumanoidViewMotion(HumanoidAMP):
         super().__init__(cfg=cfg, rl_device=rl_device, sim_device=sim_device,
                          graphics_device_id=graphics_device_id, headless=headless,
                          virtual_screen_capture=virtual_screen_capture, force_render=force_render)
-        
+
+        self._motion_dt = control_freq_inv * self.sim_params.dt
+
         num_motions = self._motion_lib.num_motions()
         self._motion_ids = torch.arange(self.num_envs, device=self.device, dtype=torch.long)
         self._motion_ids = torch.remainder(self._motion_ids, num_motions)
 
-        return
-
     def pre_physics_step(self, actions):
         self.actions = actions.to(self.device).clone()
+
+        # Set the actuation force to zero so that the motion is not affected
+        # So the action obtaining from the policy is not the real action
         forces = torch.zeros_like(self.actions)
         force_tensor = gymtorch.unwrap_tensor(forces)
         self.gym.set_dof_actuation_force_tensor(self.sim, force_tensor)
@@ -61,11 +63,11 @@ class HumanoidViewMotion(HumanoidAMP):
 
     def post_physics_step(self):
         super().post_physics_step()
-        self._motion_sync()
+        self._motion_sync()  # Read the real action from the motion data and actuate the robot
         return
-    
+
     def _get_humanoid_collision_filter(self):
-        return 1 # disable self collisions
+        return 1  # disable self collisions
 
     def _motion_sync(self):
         num_motions = self._motion_lib.num_motions()
@@ -73,19 +75,19 @@ class HumanoidViewMotion(HumanoidAMP):
         motion_times = self.progress_buf * self._motion_dt
 
         root_pos, root_rot, dof_pos, root_vel, root_ang_vel, dof_vel, key_pos \
-           = self._motion_lib.get_motion_state(motion_ids, motion_times)
-        
+            = self._motion_lib.get_motion_state(motion_ids, motion_times)
+
         root_vel = torch.zeros_like(root_vel)
         root_ang_vel = torch.zeros_like(root_ang_vel)
         dof_vel = torch.zeros_like(dof_vel)
 
         env_ids = torch.arange(self.num_envs, dtype=torch.long, device=self.device)
-        self._set_env_state(env_ids=env_ids, 
-                            root_pos=root_pos, 
-                            root_rot=root_rot, 
-                            dof_pos=dof_pos, 
-                            root_vel=root_vel, 
-                            root_ang_vel=root_ang_vel, 
+        self._set_env_state(env_ids=env_ids,
+                            root_pos=root_pos,
+                            root_rot=root_rot,
+                            dof_pos=dof_pos,
+                            root_vel=root_vel,
+                            root_ang_vel=root_ang_vel,
                             dof_vel=dof_vel)
 
         env_ids_int32 = self._humanoid_actor_ids[env_ids]
@@ -99,7 +101,8 @@ class HumanoidViewMotion(HumanoidAMP):
 
     def _compute_reset(self):
         motion_lengths = self._motion_lib.get_motion_length(self._motion_ids)
-        self.reset_buf[:], self._terminate_buf[:] = compute_view_motion_reset(self.reset_buf, motion_lengths, self.progress_buf, self._motion_dt)
+        self.reset_buf[:], self._terminate_buf[:] = compute_view_motion_reset(self.reset_buf, motion_lengths,
+                                                                              self.progress_buf, self._motion_dt)
         return
 
     def _reset_actors(self, env_ids):
@@ -108,11 +111,12 @@ class HumanoidViewMotion(HumanoidAMP):
     def _reset_env_tensors(self, env_ids):
         num_motions = self._motion_lib.num_motions()
         self._motion_ids[env_ids] = torch.remainder(self._motion_ids[env_ids] + self.num_envs, num_motions)
-        
+
         self.progress_buf[env_ids] = 0
         self.reset_buf[env_ids] = 0
         self._terminate_buf[env_ids] = 0
         return
+
 
 @torch.jit.script
 def compute_view_motion_reset(reset_buf, motion_lengths, progress_buf, dt):
