df5722ae8f512fe80145a673a8f818245264fca6
Junjia Liu <l.syj@163.com>
2023-09-09 15:57:44 +0800

    Update lqt example

--- examples/planning_control/example_lqt_cp.py
+++ examples/planning_control/example_lqt_cp.py
@@ -13,5 +13,9 @@ via_points_raw = np.load('../data/LQT_LQR/taichi_1l.npy')
 filter_indices = [i for i in range(0, len(via_points_raw) - 10, 5)]
 filter_indices.append(len(via_points_raw) - 1)
 via_points_raw = via_points_raw[filter_indices]
-u_hat, x_hat, mu, idx_slices = rf.lqt.uni_cp(via_points_raw)
+
+cfg = rf.config.utils.get_config("./planning", "lqt_cp")
+
+controller = rf.planning_control.lqt.lqt_cp.LQTCP(via_points_raw, cfg)
+u_hat, x_hat, mu, idx_slices = controller.solve()
 rf.lqt.plot_3d_uni([x_hat], mu, idx_slices)
