d5f2cf9c464f83a493ba758391247dac7f4e8f15
Junjia Liu <junjialiu@link.cuhk.edu.hk>
2023-09-12 23:44:37 +0800

    Update model params

--- examples/robolab/example_coordinate_transform.py
+++ examples/robolab/example_coordinate_transform.py
@@ -8,8 +8,13 @@ This example shows how to transform coordinates between different reference fram
 import rofunc as rf
 import numpy as np
 
-Rot_quat = rf.robolab.quaternion_about_axis(-np.pi / 2, [0, 1, 0])
-Rot_matrix = rf.robolab.homo_matrix_from_quaternion(Rot_quat)
+# Rot_quat = rf.robolab.quaternion_about_axis(-np.pi / 2, [0, 1, 0])
+# Rot_matrix = rf.robolab.homo_matrix_from_quaternion(Rot_quat)
+#
+# q = rf.robolab.quaternion_multiply([1, 0, 0, 0], Rot_quat)
+# print(q)
 
-q = rf.robolab.quaternion_multiply([1, 0, 0, 0], Rot_quat)
+a = rf.robolab.quaternion_from_euler(0, 0, np.pi/4)
+b = rf.robolab.quaternion_from_euler(0, 0, -np.pi/2)
+q = rf.robolab.quaternion_multiply([0, -0.707, 0, 0.707], a)
 print(q)
