d1a056038a251abcf2b70cbf12dc9979ae3e1512
Junjia Liu <24936522+Skylark0924@users.noreply.github.com>
2023-10-14 14:29:45 +0800

    🚀 [RofuncRL] Add pre-trained model HumanoidAMP_walk

--- examples/learning_rl/example_HumanoidAMP_RofuncRL.py
+++ examples/learning_rl/example_HumanoidAMP_RofuncRL.py
@@ -88,11 +88,11 @@ def inference(custom_args):
 
 
 if __name__ == '__main__':
-    gpu_id = 1
+    gpu_id = 0
 
     parser = argparse.ArgumentParser()
     # Available tasks: HumanoidAMP_backflip, HumanoidAMP_walk, HumanoidAMP_run, HumanoidAMP_dance, HumanoidAMP_hop
-    parser.add_argument("--task", type=str, default="HumanoidAMP_walk")
+    parser.add_argument("--task", type=str, default="HumanoidAMP_dance")
     parser.add_argument("--agent", type=str, default="amp")  # Available agent: amp
     parser.add_argument("--num_envs", type=int, default=4096)
     parser.add_argument("--sim_device", type=str, default="cuda:{}".format(gpu_id))
