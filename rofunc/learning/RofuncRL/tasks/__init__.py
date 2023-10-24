41df004bba7ddc03876385a0c007f422b6f6d858
Skylark <skylark0924@gmail.com>
2023-10-24 23:01:50 +0800

    🚀 [RofuncRL] Merge OmniIsaacGymEnv and IsaacGymEnv

--- rofunc/learning/RofuncRL/tasks/__init__.py
+++ rofunc/learning/RofuncRL/tasks/__init__.py
@@ -1,40 +1,45 @@
-# from .ant import AntTask
-# from .cartpole import CartpoleTask
-# from .curi_cabinet import CURICabinetTask
-# from .curi_cabinet_image import CURICabinetImageTask
-# from .curi_cabinet_bimanual import CURICabinetBimanualTask
-# from .curi_coffee_stirring import CURICoffeeStirringTask
-# from .franka_cabinet import FrankaCabinetTask
-# from .humanoid import HumanoidTask
-# from .humanoid_amp import HumanoidAMPTask
-# from .ase.humanoid_amp_getup import HumanoidAMPGetupTask
-# from .ase.humanoid_perturb import HumanoidPerturbTask
-# from .ase.humanoid_heading import HumanoidHeadingTask
-# from .ase.humanoid_location import HumanoidLocationTask
-# from .ase.humanoid_reach import HumanoidReachTask
-# from .ase.humanoid_strike import HumanoidStrikeTask
-# from .ase.humanoid_view_motion import HumanoidViewMotionTask
-from .omniisaacgym.ant import AntOmniTask
+class Tasks:
+    def __init__(self, env_type="isaacgym"):
+        if env_type == "isaacgym":
+            # Isaac Gym tasks
+            from .ant import AntTask
+            from .cartpole import CartpoleTask
+            from .curi_cabinet import CURICabinetTask
+            from .curi_cabinet_image import CURICabinetImageTask
+            from .curi_cabinet_bimanual import CURICabinetBimanualTask
+            from .curi_coffee_stirring import CURICoffeeStirringTask
+            from .franka_cabinet import FrankaCabinetTask
+            from .humanoid import HumanoidTask
+            from .humanoid_amp import HumanoidAMPTask
+            from .ase.humanoid_amp_getup import HumanoidAMPGetupTask
+            from .ase.humanoid_perturb import HumanoidPerturbTask
+            from .ase.humanoid_heading import HumanoidHeadingTask
+            from .ase.humanoid_location import HumanoidLocationTask
+            from .ase.humanoid_reach import HumanoidReachTask
+            from .ase.humanoid_strike import HumanoidStrikeTask
+            from .ase.humanoid_view_motion import HumanoidViewMotionTask
 
-
-task_map = {
-    # Isaac Gym tasks
-    # "Ant": AntTask,
-    # "Cartpole": CartpoleTask,
-    # "FrankaCabinet": FrankaCabinetTask,
-    # "CURICabinet": CURICabinetTask,
-    # "CURICabinetImage": CURICabinetImageTask,
-    # "CURICabinetBimanual": CURICabinetBimanualTask,
-    # "CURICoffeeStirring": CURICoffeeStirringTask,
-    # "Humanoid": HumanoidTask,
-    # "HumanoidAMP": HumanoidAMPTask,
-    # "HumanoidASEGetupSwordShield": HumanoidAMPGetupTask,
-    # "HumanoidASEPerturbSwordShield": HumanoidPerturbTask,
-    # "HumanoidASEHeadingSwordShield": HumanoidHeadingTask,
-    # "HumanoidASELocationSwordShield": HumanoidLocationTask,
-    # "HumanoidASEReachSwordShield": HumanoidReachTask,
-    # "HumanoidASEStrikeSwordShield": HumanoidStrikeTask,
-    # "HumanoidViewMotion": HumanoidViewMotionTask,
-    # OmniIsaacGym tasks
-    "AntOmni": AntOmniTask,
-}
+            self.task_map = {
+                "Ant": AntTask,
+                "Cartpole": CartpoleTask,
+                "FrankaCabinet": FrankaCabinetTask,
+                "CURICabinet": CURICabinetTask,
+                "CURICabinetImage": CURICabinetImageTask,
+                "CURICabinetBimanual": CURICabinetBimanualTask,
+                "CURICoffeeStirring": CURICoffeeStirringTask,
+                "Humanoid": HumanoidTask,
+                "HumanoidAMP": HumanoidAMPTask,
+                "HumanoidASEGetupSwordShield": HumanoidAMPGetupTask,
+                "HumanoidASEPerturbSwordShield": HumanoidPerturbTask,
+                "HumanoidASEHeadingSwordShield": HumanoidHeadingTask,
+                "HumanoidASELocationSwordShield": HumanoidLocationTask,
+                "HumanoidASEReachSwordShield": HumanoidReachTask,
+                "HumanoidASEStrikeSwordShield": HumanoidStrikeTask,
+                "HumanoidViewMotion": HumanoidViewMotionTask,
+            }
+        elif env_type == "omniisaacgym":
+            # OmniIsaacGym tasks
+            from .omniisaacgym.ant import AntOmniTask
+            self.task_map = {
+                "AntOmni": AntOmniTask,
+            }
--- rofunc/learning/utils/utils.py
+++ rofunc/learning/utils/utils.py
@@ -54,3 +54,14 @@ def set_seed(seed: Optional[int] = None, deterministic: bool = False) -> int:
         # logger.warning("PyTorch/cuDNN deterministic algorithms are enabled. This may affect performance")
 
     return seed
+
+
+def to_device(x, device):
+    if isinstance(x, torch.Tensor):
+        return x.to(device)
+    elif isinstance(x, dict):
+        return {k: to_device(v, device) for k, v in x.items()}
+    elif isinstance(x, list):
+        return [to_device(v, device) for v in x]
+    else:
+        return x
