aeb42d46dc7d8beecfa6658002874356ddbff7a9
Junjia Liu <junjialiu@link.cuhk.edu.hk>
2023-10-25 01:17:55 +0800

    🚀 [RofuncRL]  Add more OmniIsaacGymEnvs, AnymalOmni OK

--- rofunc/learning/RofuncRL/tasks/omniisaacgym/articulations/quadcopter.py
+++ rofunc/learning/RofuncRL/tasks/omniisaacgym/articulations/quadcopter.py
@@ -26,25 +26,25 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
 from typing import Optional
 
-import carb
 import numpy as np
+import torch
 from omni.isaac.core.robots.robot import Robot
 from omni.isaac.core.utils.nucleus import get_assets_root_path
 from omni.isaac.core.utils.stage import add_reference_to_stage
 
 
-class Ant(Robot):
+class Quadcopter(Robot):
     def __init__(
-            self,
-            prim_path: str,
-            name: Optional[str] = "Ant",
-            usd_path: Optional[str] = None,
-            translation: Optional[np.ndarray] = None,
-            orientation: Optional[np.ndarray] = None,
+        self,
+        prim_path: str,
+        name: Optional[str] = "Quadcopter",
+        usd_path: Optional[str] = None,
+        translation: Optional[np.ndarray] = None,
+        orientation: Optional[np.ndarray] = None,
     ) -> None:
+        """[summary]"""
 
         self._usd_path = usd_path
         self._name = name
@@ -53,14 +53,14 @@ class Ant(Robot):
             assets_root_path = get_assets_root_path()
             if assets_root_path is None:
                 carb.log_error("Could not find Isaac Sim assets folder")
-            self._usd_path = assets_root_path + "/Isaac/Robots/Ant/ant_instanceable.usd"
+            self._usd_path = assets_root_path + "/Isaac/Robots/Quadcopter/quadcopter.usd"
 
         add_reference_to_stage(self._usd_path, prim_path)
 
         super().__init__(
             prim_path=prim_path,
             name=name,
-            translation=translation,
+            position=translation,
             orientation=orientation,
             articulation_controller=None,
         )
