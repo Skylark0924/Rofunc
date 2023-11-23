9b990bea503979e342912b21af6d292a4593b17d
Junjia Liu <l.syj@163.com>
2023-11-23 16:10:40 +0800

    Fix a bug

--- rofunc/learning/RofuncRL/tasks/isaacgym/ase/humanoid_view_motion.py
+++ rofunc/learning/RofuncRL/tasks/isaacgym/ase/humanoid_view_motion.py
@@ -33,16 +33,16 @@ from isaacgym import gymtorch
 from rofunc.learning.RofuncRL.tasks.isaacgym.ase.humanoid_amp import HumanoidAMP
 
 
-class HumanoidViewMotionTask(HumanoidAMP):
+class HumanoidASEViewMotionTask(HumanoidAMP):
     def __init__(
-        self,
-        cfg,
-        rl_device,
-        sim_device,
-        graphics_device_id,
-        headless,
-        virtual_screen_capture,
-        force_render,
+            self,
+            cfg,
+            rl_device,
+            sim_device,
+            graphics_device_id,
+            headless,
+            virtual_screen_capture,
+            force_render,
     ):
         self.cfg = cfg
         control_freq_inv = cfg["env"]["controlFrequencyInv"]
