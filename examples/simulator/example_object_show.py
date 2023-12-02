24ce320df472b883d6631cd995f500ce11a31e53
Junjia Liu <l.syj@163.com>
2023-12-03 01:26:56 +0800

    Update

--- examples/simulator/example_object_show.py
+++ examples/simulator/example_object_show.py
@@ -1,23 +1,24 @@
 """
-CURI Interactive Mode
+Object Simulator and Camera Tracking
 ============================================================
 
-Show the interactive mode of the CURI simulator.
+Example for showing objects in the simulator and tracking cameras with rgb, depth, and segmentation modes.
 """
+
 import isaacgym
 import rofunc as rf
 
 args = rf.config.get_sim_config("Objects")
 asset_files = ["urdf/ycb/002_master_chef_can/002_master_chef_can.urdf",
-                "urdf/ycb/003_cracker_box/003_cracker_box.urdf",
-                "urdf/ycb/004_sugar_box/004_sugar_box.urdf",
-                "urdf/ycb/005_tomato_soup_can/005_tomato_soup_can.urdf",
-                "urdf/ycb/006_mustard_bottle/006_mustard_bottle.urdf",
-                "urdf/ycb/007_tuna_fish_can/007_tuna_fish_can.urdf",
-                "urdf/ycb/008_pudding_box/008_pudding_box.urdf",
-                "urdf/ycb/009_gelatin_box/009_gelatin_box.urdf",
-                "urdf/ycb/010_potted_meat_can/010_potted_meat_can.urdf",
-                "urdf/ycb/011_banana/011_banana.urdf",
+               "urdf/ycb/003_cracker_box/003_cracker_box.urdf",
+               "urdf/ycb/004_sugar_box/004_sugar_box.urdf",
+               "urdf/ycb/005_tomato_soup_can/005_tomato_soup_can.urdf",
+               "urdf/ycb/006_mustard_bottle/006_mustard_bottle.urdf",
+               "urdf/ycb/007_tuna_fish_can/007_tuna_fish_can.urdf",
+               "urdf/ycb/008_pudding_box/008_pudding_box.urdf",
+               "urdf/ycb/009_gelatin_box/009_gelatin_box.urdf",
+               "urdf/ycb/010_potted_meat_can/010_potted_meat_can.urdf",
+               "urdf/ycb/011_banana/011_banana.urdf",
                ]
 object_sim = rf.sim.ObjectSim(args, asset_file=asset_files)
 object_sim.create_track_cameras()
