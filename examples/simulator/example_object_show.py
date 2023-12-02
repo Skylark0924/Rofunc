47f502053851691b27b4d23afd7157e656e41b0c
Junjia Liu <l.syj@163.com>
2023-12-02 16:00:23 +0800

    Collision model ok

--- examples/simulator/example_object_show.py
+++ examples/simulator/example_object_show.py
@@ -13,6 +13,12 @@ asset_files = ["urdf/ycb/002_master_chef_can/002_master_chef_can.urdf",
                 "urdf/ycb/004_sugar_box/004_sugar_box.urdf",
                 # "urdf/ycb/005_tomato_soup_can/005_tomato_soup_can.urdf",
                 # "urdf/ycb/006_mustard_bottle/006_mustard_bottle.urdf",
+                "urdf/ycb/007_tuna_fish_can/007_tuna_fish_can.urdf",
+                "urdf/ycb/008_pudding_box/008_pudding_box.urdf",
+                "urdf/ycb/009_gelatin_box/009_gelatin_box.urdf",
+                "urdf/ycb/010_potted_meat_can/010_potted_meat_can.urdf",
+                "urdf/ycb/011_banana/011_banana.urdf",
                ]
+# asset_files = "urdf/ycb/002_master_chef_can/002_master_chef_can.urdf"
 object_sim = rf.sim.ObjectSim(args, asset_file=asset_files)
 object_sim.show()
