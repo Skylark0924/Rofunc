ecfbbb0b47c3750e410249386adf4c83fc14d5e5
Junjia Liu <l.syj@163.com>
2023-12-26 13:54:42 +0800

    🚀 [RofuncRL] Add four dexterous task pre-trained models to ModelZoo
    BiShadowHandCatchOver2Underarm, BiShadowHandCatchUnderarm, BiShadowHandDoorOpenInward, BiShadowHandDoorOpenOutward

--- examples/learning_rl/IsaacGym_RofuncRL/example_DexterousHands_RofuncRL.py
+++ examples/learning_rl/IsaacGym_RofuncRL/example_DexterousHands_RofuncRL.py
@@ -76,10 +76,11 @@ def inference(custom_args):
     trainer = Trainers().trainer_map[custom_args.agent](cfg=cfg.train,
                                                         env=infer_env,
                                                         device=cfg.rl_device,
-                                                        env_name=custom_args.task)
+                                                        env_name=custom_args.task,
+                                                        inference=True)
     # load checkpoint
     if custom_args.ckpt_path is None:
-        custom_args.ckpt_path = model_zoo(name="AntRofuncRLPPO.pth")
+        custom_args.ckpt_path = model_zoo(name=f"{custom_args.task}RofuncRLPPO.pth")
     trainer.agent.load_ckpt(custom_args.ckpt_path)
 
     # Start inference
@@ -96,13 +97,13 @@ if __name__ == '__main__':
     #                  BiShadowHandGraspAndPlace, BiShadowHandLiftUnderarm, BiShadowHandPen, BiShadowHandPointCloud,
     #                  BiShadowHandPushBlock, BiShadowHandReOrientation, BiShadowHandScissors, BiShadowHandSwingCup,
     #                  BiShadowHandSwitch, BiShadowHandTwoCatchUnderarm
-    parser.add_argument("--task", type=str, default="BiShadowHandOver")
+    parser.add_argument("--task", type=str, default="BiShadowHandCatchUnderarm")
     parser.add_argument("--agent", type=str, default="ppo")  # Available agents: ppo, sac, td3, a2c
-    parser.add_argument("--num_envs", type=int, default=4096)
+    parser.add_argument("--num_envs", type=int, default=256)
     parser.add_argument("--sim_device", type=int, default=0)
     parser.add_argument("--rl_device", type=int, default=gpu_id)
     parser.add_argument("--headless", type=str, default="True")
-    parser.add_argument("--inference", action="store_true", help="turn to inference mode while adding this argument")
+    parser.add_argument("--inference", action="store_false", help="turn to inference mode while adding this argument")
     parser.add_argument("--ckpt_path", type=str, default=None)
     custom_args = parser.parse_args()
 
