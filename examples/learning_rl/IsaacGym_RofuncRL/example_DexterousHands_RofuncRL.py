3f91f3b3dba7390b2b89d331ab2dbe5f26ad9ee5
Junjia Liu <l.syj@163.com>
2023-12-26 23:38:30 +0800

    🚀 [RofuncRL] Add two more dexterous task pre-trained models to ModelZoo

--- examples/learning_rl/IsaacGym_RofuncRL/example_DexterousHands_RofuncRL.py
+++ examples/learning_rl/IsaacGym_RofuncRL/example_DexterousHands_RofuncRL.py
@@ -97,7 +97,7 @@ if __name__ == '__main__':
     #                  BiShadowHandGraspAndPlace, BiShadowHandLiftUnderarm, BiShadowHandPen, BiShadowHandPointCloud,
     #                  BiShadowHandPushBlock, BiShadowHandReOrientation, BiShadowHandScissors, BiShadowHandSwingCup,
     #                  BiShadowHandSwitch, BiShadowHandTwoCatchUnderarm
-    parser.add_argument("--task", type=str, default="BiShadowHandSwingCup")
+    parser.add_argument("--task", type=str, default="BiShadowHandSwitch")
     parser.add_argument("--agent", type=str, default="ppo")  # Available agents: ppo, sac, td3, a2c
     parser.add_argument("--num_envs", type=int, default=256)
     parser.add_argument("--sim_device", type=int, default=0)
