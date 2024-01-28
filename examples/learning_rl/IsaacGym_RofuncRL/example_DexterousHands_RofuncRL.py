7ae14db745a2cf6645bd4f9e31ee917467957894
Junjia Liu <l.syj@163.com>
2024-01-28 17:51:00 +0800

    🚀 [RofuncRL] CURI Synergy-based Softhand grasping tasks are supported

--- examples/learning_rl/IsaacGym_RofuncRL/example_DexterousHands_RofuncRL.py
+++ examples/learning_rl/IsaacGym_RofuncRL/example_DexterousHands_RofuncRL.py
@@ -7,6 +7,7 @@ Examples of learning hand (Shadow Hand, Allegro Hand, qbSofthand) tasks by Rofun
 
 import isaacgym
 import argparse
+import datetime
 
 from rofunc.config.utils import omegaconf_to_dict, get_config
 from rofunc.learning.RofuncRL.tasks import Tasks
@@ -26,9 +27,10 @@ def train(custom_args):
                       "headless={}".format(custom_args.headless),
                       "num_envs={}".format(custom_args.num_envs)]
     cfg = get_config('./learning/rl', 'config', args=args_overrides)
-    cfg.task.env.objectType = custom_args.object.lower()
+    if custom_args.task == "CURIQbSoftHandSynergyGrasp":
+        cfg.task.env.objectType = custom_args.object.lower()
     cfg.train.Trainer.maximum_steps = 100000
-    cfg.train.Trainer.experiment_name = custom_args.object
+    cfg.train.Trainer.experiment_name = "{}_{}".format(custom_args.object, datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S-%f"))
     cfg_dict = omegaconf_to_dict(cfg.task)
 
     set_seed(cfg.train.Trainer.seed)
@@ -47,6 +49,8 @@ def train(custom_args):
                                                         env=env,
                                                         device=cfg.rl_device,
                                                         env_name=custom_args.task)
+    if custom_args.ckpt_path is not None:
+        trainer.agent.load_ckpt(custom_args.ckpt_path)
 
     # Start training
     trainer.train()
@@ -59,8 +63,13 @@ def inference(custom_args):
                       "device_id={}".format(custom_args.sim_device),
                       "rl_device=cuda:{}".format(custom_args.rl_device),
                       "headless={}".format(False),
-                      "num_envs={}".format(16)]
+                      "num_envs={}".format(1)]
     cfg = get_config('./learning/rl', 'config', args=args_overrides)
+    if custom_args.task == "CURIQbSoftHandSynergyGrasp":
+        cfg.task.env.objectType = custom_args.object.lower()
+    cfg.train.Trainer.experiment_name = custom_args.object
+    cfg.train.Trainer.inference_steps = 10000
+
     cfg_dict = omegaconf_to_dict(cfg.task)
 
     set_seed(cfg.train.Trainer.seed)
@@ -102,13 +111,17 @@ if __name__ == '__main__':
     #                  QbSoftHandGrasp, BiQbSoftHandGraspAndPlace, BiQbSoftHandSynergyGrasp, QbSoftHandSynergyGrasp
     #                  ShadowHandGrasp, CURIQbSoftHandSynergyGrasp
     parser.add_argument("--task", type=str, default="CURIQbSoftHandSynergyGrasp")
-    parser.add_argument("--object", type=str, default="Hammer")
+    # Available objects: Hammer, Spatula, Large_Clamp, Mug, Power_Drill, Knife, Scissors, Large_Marker, Phillips_Screw_Driver
+    # Only for CURIQbSoftHandSynergyGrasp
+    parser.add_argument("--object", type=str, default="Mug")
+    # Only for CURIQbSoftHandSynergyGrasp
+    parser.add_argument("--use_synergy", type=str, default="True")
     parser.add_argument("--agent", type=str, default="ppo")  # Available agents: ppo, sac, td3, a2c
     parser.add_argument("--num_envs", type=int, default=1024)
     parser.add_argument("--sim_device", type=int, default=0)
     parser.add_argument("--rl_device", type=int, default=gpu_id)
     parser.add_argument("--headless", type=str, default="False")
-    parser.add_argument("--inference", action="store_false", help="turn to inference mode while adding this argument")
+    parser.add_argument("--inference", action="store_true", help="turn to inference mode while adding this argument")
     parser.add_argument("--ckpt_path", type=str, default=None)
     custom_args = parser.parse_args()
 
