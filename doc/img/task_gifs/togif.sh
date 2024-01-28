36b5dcdba29cb01d02d9cccfef75a3244a2c9a9d
Junjia Liu <l.syj@163.com>
2024-01-29 01:50:37 +0800

    📚 Reformat gifs

---
+++ doc/img/task_gifs/togif.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+FILES="/home/ubuntu/Github/Rofunc/doc/img/task_gifs/*.gif"
+for filename in $FILES; do
+    echo "Processing $filename file..."
+    ffmpeg -i $filename -vf crop=640*ih/480:ih:iw/2-320*ih/480:0 -r 10 -b:v 5000k ../task_gif2/$(basename -- "$filename")
+done
