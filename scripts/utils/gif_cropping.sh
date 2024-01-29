e3b16c2a91552a3f2f715aef96b884556e70920b
Junjia Liu <l.syj@163.com>
2024-01-29 16:38:57 +0800

    📚 Reformat gifs

--- scripts/utils/gif_cropping.sh
+++ scripts/utils/gif_cropping.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-FILES="/home/ubuntu/Github/Rofunc/doc/img/task_gif/*.gif"
+FILES="/home/ubuntu/Github/Rofunc/doc/img/task_gifs/*.gif"
 for filename in $FILES; do
     echo "Processing $filename file..."
-    ffmpeg -i $filename -vf crop=640*ih/480:ih:iw/2-320*ih/480:0 -r 10 -b:v 5000k ../task_gif2/$(basename -- "$filename")
+    ffmpeg -i $filename -vf crop=640*ih/480:ih:iw/2-320*ih/480:0 -r 10 -b:v 5000k ../task_gifs2/$(basename -- "$filename")
 done
