f67127843b5c815f63ff8d70d7a70a8b2e05acf0
Junjia Liu <l.syj@163.com>
2024-01-29 16:08:50 +0800

    📚 Reformat gifs

--- scripts/utils/gif_watermark.sh
+++ scripts/utils/gif_watermark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-FILES="/home/ubuntu/Github/Rofunc/doc/img/task_gif/*.gif"
+FILES="/home/ubuntu/Github/Rofunc/doc/img/task_gifs/*.gif"
 for filename in $FILES; do
     echo "Processing $filename file..."
-    ffmpeg -hide_banner -i $filename -i /home/ubuntu/Github/Rofunc/doc/img/logo/logo2_nb.png -filter_complex "[1:v] scale=-1:80 [logo];[0:v][logo]overlay=x=W-w-10:y=H-h-10" /home/ubuntu/Github/Rofunc/doc/img/task_gif2/$(basename -- "$filename") -y
+    ffmpeg -hide_banner -i $filename -i /home/ubuntu/Github/Rofunc/doc/img/logo/logo2_nb.png -filter_complex "[1:v] scale=-1:80 [logo];[0:v][logo]overlay=x=W-w-10:y=H-h-10" /home/ubuntu/Github/Rofunc/doc/img/task_gifs2/$(basename -- "$filename") -y
 done
