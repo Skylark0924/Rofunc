55072b40a04b9cb3a4525e463d1054500e45f021
Junjia Liu <l.syj@163.com>
2024-03-11 00:23:21 +0800

    Add humanoidphyshoi task

--- examples/learning_rl/IsaacGym_RofuncRL/example_HumanoidPhysHOI_RofuncRL.py
+++ examples/learning_rl/IsaacGym_RofuncRL/example_HumanoidPhysHOI_RofuncRL.py
@@ -25,7 +25,7 @@ def train(custom_args):
                       "headless={}".format(custom_args.headless),
                       "num_envs={}".format(custom_args.num_envs)]
     cfg = get_config('./learning/rl', 'config', args=args_overrides)
-    cfg.task.env.motion_file = f'amp_humanoid_{motion_file}.npy'
+    cfg.task.env.motion_file = args.motion_file
     cfg_dict = omegaconf_to_dict(cfg.task)
 
     set_seed(cfg.train.Trainer.seed)
@@ -91,9 +91,12 @@ if __name__ == '__main__':
     gpu_id = 0
 
     parser = argparse.ArgumentParser()
-    # Available tasks: HumanoidAMP_backflip, HumanoidAMP_walk, HumanoidAMP_run, HumanoidAMP_dance, HumanoidAMP_hop
-    parser.add_argument("--task", type=str, default="HumanoidAMP_hop")
-    parser.add_argument("--agent", type=str, default="amp")  # Available agent: amp
+    # Available tasks: HumanoidPhyshoi
+    parser.add_argument("--task", type=str, default="HumanoidPhyshoi")
+    # Available motion files: backdribble, backspin, changeleg, fingerspin, pass, rebound, toss, walkpick
+    parser.add_argument("--motion_file", type=str, default="examples/data/ballplay/backdribble.pt")
+    # Available agent: physhoi
+    parser.add_argument("--agent", type=str, default="physhoi")
     parser.add_argument("--num_envs", type=int, default=4096)
     parser.add_argument("--sim_device", type=int, default=0)
     parser.add_argument("--rl_device", type=int, default=gpu_id)
