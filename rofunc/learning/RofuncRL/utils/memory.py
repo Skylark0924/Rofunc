f9affc5be1aaa68d642d84dcf822fff1f37d7e11
Junjia Liu <l.syj@163.com>
2024-04-02 23:42:03 +0800

    🚀 [RofuncRL] Change motion dataset setup in AMP and change the memory

--- rofunc/learning/RofuncRL/utils/memory.py
+++ rofunc/learning/RofuncRL/utils/memory.py
@@ -318,6 +318,95 @@ class Memory:
             if self.export:
                 self.save(directory=self.export_directory, format=self.export_format)
 
+    def add_samples_by_name(self, name, tensor) -> None:
+        """Record samples in memory
+
+        Samples should be a tensor with 2-components shape (number of environments, data size).
+        All tensors must be of the same shape
+
+        According to the number of environments, the following classification is made:
+
+        - one environment:
+          Store a single sample (tensors with one dimension) and increment the environment index (second index) by one
+
+        - number of environments less than num_envs:
+          Store the samples and increment the environment index (second index) by the number of the environments
+
+        - number of environments equals num_envs:
+          Store the samples and increment the memory index (first index) by one
+
+        :param tensors: Sampled data as key-value arguments where the keys are the names of the tensors to be modified.
+                        Non-existing tensors will be skipped
+        :type tensors: dict
+
+        :raises ValueError: No tensors were provided or the tensors have incompatible shapes
+        """
+        # if not tensors:
+        #     raise ValueError(
+        #         "No samples to be recorded in memory. Pass samples as key-value arguments (where key is the tensor name)")
+
+        # dimensions and shapes of the tensors (assume all tensors have the dimensions of the first tensor)
+        # tmp = tensors.get("states", tensors[next(iter(tensors))])  # ask for states first
+        # dim, shape = tmp.ndim, tmp.shape
+
+        dim, shape = tensor.ndim, tensor.shape
+
+        # multi environment (number of environments equals num_envs)
+        if dim >= 2 and shape[0] == self.num_envs:
+            try:
+                if name in self.tensors:
+                    self.tensors[name][self.memory_index].copy_(tensor)
+            except:
+                try:
+                    raise ValueError("The tensors have incompatible shapes. \n name: {} \n Expect shape: {} "
+                                     "\n Got shape: {}".format(name, self.tensors[name][self.memory_index].shape,
+                                                               tensor.shape))
+                except:
+                    raise ValueError("The tensor might be None. \n name: {}".format(name))
+            self.memory_index += 1
+        # # multi environment (number of environments less than num_envs)
+        # elif dim >= 2 and shape[0] < self.num_envs:
+        #     for name, tensor in tensors.items():
+        #         if name in self.tensors:
+        #             self.tensors[name][self.memory_index, self.env_index:self.env_index + tensor.shape[0]].copy_(tensor)
+        #     self.env_index += tensor.shape[0]
+        # # single environment - multi sample (number of environments greater than num_envs (num_envs = 1))
+        # elif dim >= 2 and self.num_envs == 1:
+        #     for name, tensor in tensors.items():
+        #         if name in self.tensors:
+        #             num_samples = min(shape[0], self.memory_size - self.memory_index)
+        #             remaining_samples = shape[0] - num_samples
+        #             # copy the first n samples
+        #             self.tensors[name][self.memory_index:self.memory_index + num_samples].copy_(
+        #                 tensor[:num_samples].unsqueeze(dim=1))
+        #             self.memory_index += num_samples
+        #             # storage remaining samples
+        #             if remaining_samples > 0:
+        #                 self.tensors[name][:remaining_samples].copy_(tensor[num_samples:].unsqueeze(dim=1))
+        #                 self.memory_index = remaining_samples
+        # # single environment (TODO: cant handle image observation)
+        # elif dim == 1:
+        #     for name, tensor in tensors.items():
+        #         if name in self.tensors:
+        #             self.tensors[name][self.memory_index, self.env_index].copy_(tensor)
+        #     self.env_index += 1
+        else:
+            raise ValueError(
+                "Expected tensors with 2-components shape (number of environments = {}, data size), got {}".format(
+                    self.num_envs, shape))
+
+        # update indexes and flags
+        if self.env_index >= self.num_envs:
+            self.env_index = 0
+            self.memory_index += 1
+        if self.memory_index >= self.memory_size:
+            self.memory_index = 0
+            self.filled = True
+
+            # export tensors to file
+            if self.export:
+                self.save(directory=self.export_directory, format=self.export_format)
+
     def sample(self,
                names: Union[Tuple[str], List[str]],
                batch_size: int,
