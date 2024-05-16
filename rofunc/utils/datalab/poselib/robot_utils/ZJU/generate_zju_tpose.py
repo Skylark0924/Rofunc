f0352d63afefa99321f12d03020a877d7c1ef338
Junjia Liu <l.syj@163.com>
2024-05-17 00:49:55 +0800

    🚀 [RofuncRL] Finish ZJUHumanoid retargeting

--- rofunc/utils/datalab/poselib/robot_utils/ZJU/generate_zju_tpose.py
+++ rofunc/utils/datalab/poselib/robot_utils/ZJU/generate_zju_tpose.py
@@ -80,6 +80,6 @@ def get_tpose(xml_path, save_path, verbose=True):
 
 if __name__ == '__main__':
     rofunc_path = rf.oslab.get_rofunc_path()
-    xml_path = os.path.join(rofunc_path, "simulator/assets/mjcf/zju_humanoid/zju_humanoid.xml")
-    save_path = os.path.join(rofunc_path, "utils/datalab/poselib/data/target_zju_humanoid_tpose.npy")
+    xml_path = os.path.join(rofunc_path, "simulator/assets/mjcf/zju_humanoid/zju_humanoid_w_qbhand.xml")
+    save_path = os.path.join(rofunc_path, "utils/datalab/poselib/data/target_zju_humanoid_w_qbhand_tpose.npy")
     get_tpose(xml_path, save_path)
