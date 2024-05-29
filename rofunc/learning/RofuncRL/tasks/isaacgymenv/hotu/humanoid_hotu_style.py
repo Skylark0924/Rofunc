8aea0d640c33fb9e6e3b5106718ca327001f2224
Junjia Liu <l.syj@163.com>
2024-05-29 17:29:13 +0800

    Update

--- rofunc/learning/RofuncRL/tasks/isaacgymenv/hotu/humanoid_hotu_style.py
+++ rofunc/learning/RofuncRL/tasks/isaacgymenv/hotu/humanoid_hotu_style.py
@@ -28,55 +28,5 @@ class HumanoidHOTUStyleTask(HumanoidHOTUHRLTask):
                          graphics_device_id=graphics_device_id, headless=headless,
                          virtual_screen_capture=virtual_screen_capture, force_render=force_render)
 
-    def get_obs_size(self):
-        obs_size = super().get_obs_size()
-        if self._enable_task_obs:
-            task_obs_size = self.get_task_obs_size()
-            obs_size += task_obs_size
-        return obs_size
-
-    def get_task_obs_size(self):
-        return 0
-
-    def pre_physics_step(self, actions):
-        super().pre_physics_step(actions)
-        self._update_task()
-
-    def render(self, sync_frame_time=False):
-        super().render(sync_frame_time)
-
-        if self.viewer:
-            self._draw_task()
-
-    def _update_task(self):
-        return
-
-    def reset_idx(self, env_ids):
-        super().reset_idx(env_ids)
-        self._reset_task(env_ids)
-
-    def _reset_task(self, env_ids):
-        pass
-
-    def _compute_observations(self, env_ids=None):
-        humanoid_obs = self._compute_humanoid_obs(env_ids)
-
-        if self._enable_task_obs:
-            task_obs = self._compute_task_obs(env_ids)
-            obs = torch.cat([humanoid_obs, task_obs], dim=-1)
-        else:
-            obs = humanoid_obs
-
-        if env_ids is None:
-            self.obs_buf[:] = obs
-        else:
-            self.obs_buf[env_ids] = obs
-
-    def _compute_task_obs(self, env_ids=None):
-        return NotImplemented
-
     def _compute_reward(self, actions):
         self.rew_buf[:] = compute_humanoid_reward(self.obs_buf)
-
-    def _draw_task(self):
-        pass
