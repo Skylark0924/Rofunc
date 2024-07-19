c9d5b12091c7ffbab2e496a6beddfec7fd98bbdb
Junjia Liu <l.syj@163.com>
2024-07-19 22:14:01 +0800

    🏄‍♂️ Remove pinocchio installation

--- scripts/install_w_baselines.sh
+++ scripts/install_w_baselines.sh
@@ -6,17 +6,17 @@ alias py3rf="$HOME/anaconda3/envs/rofunc/bin/python3.8"
 sudo apt-get install swig
 
 # Install pinocchio
-sudo apt install -qqy lsb-release gnupg2 curl
-echo "deb [arch=amd64] http://robotpkg.openrobots.org/packages/debian/pub $(lsb_release -cs) robotpkg" | sudo tee /etc/apt/sources.list.d/robotpkg.list
-curl http://robotpkg.openrobots.org/packages/debian/robotpkg.key | sudo apt-key add -
-sudo apt-get update
-sudo apt install -qqy robotpkg-py3*-pinocchio
-
-export PATH=/opt/openrobots/bin:$PATH
-export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=/opt/openrobots/lib/python3.10/site-packages:$PYTHONPATH # Adapt your desired python version here
-export CMAKE_PREFIX_PATH=/opt/openrobots:$CMAKE_PREFIX_PATH
+#sudo apt install -qqy lsb-release gnupg2 curl
+#echo "deb [arch=amd64] http://robotpkg.openrobots.org/packages/debian/pub $(lsb_release -cs) robotpkg" | sudo tee /etc/apt/sources.list.d/robotpkg.list
+#curl http://robotpkg.openrobots.org/packages/debian/robotpkg.key | sudo apt-key add -
+#sudo apt-get update
+#sudo apt install -qqy robotpkg-py3*-pinocchio
+#
+#export PATH=/opt/openrobots/bin:$PATH
+#export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH
+#export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
+#export PYTHONPATH=/opt/openrobots/lib/python3.10/site-packages:$PYTHONPATH # Adapt your desired python version here
+#export CMAKE_PREFIX_PATH=/opt/openrobots:$CMAKE_PREFIX_PATH
 
 # Downgrade pip to 21.0 for avoiding useless installation of different versions
 pip3rf install pip==21.3.1
