f5e85144347d28c06d2092f726506ecd2b683066
Junjia Liu <l.syj@163.com>
2024-07-19 22:49:29 +0800

    🦾 New forward kinematics

--- rofunc/utils/robolab/kinematics/kinpy_utils.py
+++ rofunc/utils/robolab/kinematics/kinpy_utils.py
@@ -1,4 +1,9 @@
-from rofunc.utils.robolab.kinematics.mjcf import build_chain_from_mjcf
+from rofunc.utils.oslab.path import check_package_exist
+from rofunc.utils.logger.beauty_logger import beauty_print
+
+check_package_exist("kinpy")
+
+import kinpy as kp
 
 
 def build_chain_from_model(model_path: str, verbose=False):
@@ -9,15 +14,17 @@ def build_chain_from_model(model_path: str, verbose=False):
     :param verbose: whether to print the chain
     :return: robot kinematics chain
     """
-    import pytorch_kinematics as pk
 
     if model_path.endswith(".urdf"):
-        chain = pk.build_chain_from_urdf(open(model_path).read())
+        chain = kp.build_chain_from_urdf(open(model_path).read())
     elif model_path.endswith(".xml"):
-        chain = build_chain_from_mjcf(model_path)
+        chain = kp.build_chain_from_mjcf(open(model_path).read())
     else:
         raise ValueError("Invalid model path")
 
     if verbose:
+        beauty_print("Robot chain:")
         print(chain)
+        beauty_print("Robot joints:")
+        print(chain.get_joint_parameter_names())
     return chain
