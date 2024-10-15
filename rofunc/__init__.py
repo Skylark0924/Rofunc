1d1e531546aef1719173813e1fc1278208d9ab9b
Junjia Liu <l.syj@163.com>
2024-10-15 19:16:10 +0800

    🐛 Fix some bugs

--- rofunc/__init__.py
+++ rofunc/__init__.py
@@ -19,11 +19,11 @@ warnings.simplefilter('ignore', DeprecationWarning)
 
 from .devices import zed, xsens, optitrack, mmodal, emg
 from . import simulator as sim
-# from .learning import ml
+from .learning import ml
 from .learning import RofuncIL, RofuncRL
-# from .planning_control import lqt, lqr
+from .planning_control import lqt, lqr
 from .utils import visualab, robolab, logger, oslab
 from .utils.datalab import primitive, data_generator
 from . import config
 
-# from .learning.ml import tpgmm, gmr, tpgmr
+from .learning.ml import tpgmm, gmr, tpgmr
